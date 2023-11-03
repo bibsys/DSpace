7ca97e0764e8b3b9f7e72d4e77186056f220fffd
MichaëlP <michael.pourbaix@uclouvain.be>
2023-11-03 09:41:13 +0100

    [ADD] feat(US-Widget): UCLouvain Authority, Dilbert & OSIS API handlers for new dynamic fields functionnality
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>
    Co-authored-by: Laurent Dubois <laurent.dubois@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/authority/configuration/UCLouvainAuthorAuthorityAPIConfiguration.java
@@ -0,0 +1,22 @@
+package org.dspace.uclouvain.authority.configuration;
+
+public class UCLouvainAuthorAuthorityAPIConfiguration {
+    private String filter;
+    private String authorityName;
+
+    public void setFilter(String filter){
+        this.filter = filter;
+    }
+
+    public String getFilter(){
+        return this.filter;
+    }
+
+    public void setAuthorityName(String authorityName){
+        this.authorityName = authorityName;
+    }
+
+    public String getAuthorityName(){
+        return this.authorityName;
+    }
+}
