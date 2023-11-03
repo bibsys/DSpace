69424f95a930193fa64b982c2a821c2c6a85a25c
MichaëlP <michael.pourbaix@uclouvain.be>
2023-11-03 09:41:13 +0100

    [ADD] feat(US-Widget): UCLouvain Authority, Dilbert & OSIS API handlers for new dynamic fields functionnality
    authored-by: Michael Pourbaix <michael.pourbaix@uclouvain.be>
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>
    Co-authored-by: Laurent Dubois <laurent.dubois@uclouvain.be>

---
+++ dspace-uclouvain/src/main/java/org/dspace/app/configuration/UCLouvainConfiguration.java
@@ -0,0 +1,10 @@
+package org.dspace.app.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan({"org.dspace.uclouvain"})
+public class UCLouvainConfiguration {
+}
