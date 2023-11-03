dcbca60a5a59fc6b653b73210eec43ca65b44b8a
MichaëlP <michael.pourbaix@uclouvain.be>
2023-11-03 09:41:13 +0100

    [ADD] feat(widget): UCLouvain Authority, Dilbert & OSIS API handlers for new dynamic fields functionnality
    Co-authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/authority/client/UCLouvainAuthorAuthorityClient.java
@@ -0,0 +1,14 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.authority.client;
+
+import org.dspace.uclouvain.external.dilbert.model.DialPerson;
+
+public interface UCLouvainAuthorAuthorityClient {
+    DialPerson[] getStudentByTermWithFilter(String query);
+}
