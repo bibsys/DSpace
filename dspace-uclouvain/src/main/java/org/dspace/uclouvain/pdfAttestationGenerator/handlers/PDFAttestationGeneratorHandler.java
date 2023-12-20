ab483bdeb7eb5124a5c3569728b44eac20ff2bf4
MichaëlP <michael.pourbaix@uclouvain.be>
2023-12-20 09:57:23 +0100

    [ADD] feat(pdf-attestation): Emailing system to send attestation on final workflow validation
    Co-authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-uclouvain/src/main/java/org/dspace/uclouvain/pdfAttestationGenerator/handlers/PDFAttestationGeneratorHandler.java
+++ dspace-uclouvain/src/main/java/org/dspace/uclouvain/pdfAttestationGenerator/handlers/PDFAttestationGeneratorHandler.java
@@ -1,5 +1,6 @@
 package org.dspace.uclouvain.pdfAttestationGenerator.handlers;
 
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 
@@ -8,4 +9,5 @@ import org.dspace.uclouvain.pdfAttestationGenerator.exceptions.PDFGenerationExce
 
 public interface PDFAttestationGeneratorHandler {
     public void getAttestation(OutputStream out, UUID uuid) throws PDFGenerationException;
+    public InputStream getAttestationAsInputStream(UUID uuid) throws PDFGenerationException;
 }
