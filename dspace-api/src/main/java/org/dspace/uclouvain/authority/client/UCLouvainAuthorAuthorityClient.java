214a7aa7cda59268349189ccdf7d8debed6b02c0
MichaëlP <michael.pourbaix@uclouvain.be>
2024-01-23 16:31:32 +0100

    [UPD] feat(submission-form): Tweaking UCLouvain's authorities + new PromoterAuthority

--- dspace-api/src/main/java/org/dspace/uclouvain/authority/client/UCLouvainAuthorAuthorityClient.java
+++
@@ -1,7 +0,0 @@
-package org.dspace.uclouvain.authority.client;
-
-import org.dspace.uclouvain.external.dilbert.model.DialPerson;
-
-public interface UCLouvainAuthorAuthorityClient {
-    public DialPerson[] getStudentByTermWithFilter(String query);
-}
