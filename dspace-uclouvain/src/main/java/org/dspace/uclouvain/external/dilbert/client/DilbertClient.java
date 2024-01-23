d4e88487a278ff6d2002617902dbd5c92bfaaa1a
MichaëlP <michael.pourbaix@uclouvain.be>
2024-01-23 16:31:32 +0100

    [UPD] feat(submission-form): Tweaking UCLouvain's authorities + new PromoterAuthority

--- dspace-uclouvain/src/main/java/org/dspace/uclouvain/external/dilbert/client/DilbertClient.java
+++ dspace-uclouvain/src/main/java/org/dspace/uclouvain/external/dilbert/client/DilbertClient.java
@@ -1,42 +1,47 @@
 package org.dspace.uclouvain.external.dilbert.client;
 
-import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 import org.dspace.uclouvain.external.dilbert.model.DialPerson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.dspace.uclouvain.authority.client.UCLouvainAuthorAuthorityClient;
-import org.dspace.uclouvain.authority.configuration.UCLouvainAuthorAuthorityAPIConfiguration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dspace.uclouvain.authority.client.UCLouvainAuthorityClient;
+import org.dspace.uclouvain.authority.configuration.UCLouvainAuthorityAPIConfiguration;
 import org.dspace.uclouvain.core.GenericHttpClient;
 import org.dspace.uclouvain.core.GenericResponse;
 
 @Service
-public class DilbertClient implements UCLouvainAuthorAuthorityClient {
+public class DilbertClient implements UCLouvainAuthorityClient {
 
-    private UCLouvainAuthorAuthorityAPIConfiguration uclouvainAuthorityConfiguration;
+    private static Logger logger = LogManager.getLogger(DilbertClient.class);
+
+    private UCLouvainAuthorityAPIConfiguration uclouvainAuthorityConfiguration;
     private GenericHttpClient httpClient;
 
     /** 
-     * Base UCLouvainClient Constructor, instantiate HttpClient & UCLouvainAuthorityConfiguration
+     * Base UCLouvainClient Constructor, instantiate HttpClient & UCLouvainAuthorityConfiguration.
      */
     @Autowired
-    public DilbertClient(UCLouvainAuthorAuthorityAPIConfiguration uclouvainAuthorityConfiguration) {
+    public DilbertClient(UCLouvainAuthorityAPIConfiguration uclouvainAuthorityConfiguration) {
         this.uclouvainAuthorityConfiguration = uclouvainAuthorityConfiguration;
     }
-    
+
     /** 
-     * Search by student first name or/and second name and filtering by {filter} (ex: filter="student")
-     * It returns a list of student information
+     * Generic method to call the Dilbert API with a given term && filter.
      * 
-     * @param term
-     * @param filter
-     * @return: A list of dial peron information 
+     * @param term: The name and/or second name of the person.
+     * @param filterKey: The filter key which indicate the type of person to extract (ex: student, promoter...)
+     * @return: A list of dial person information.
      */
-    public DialPerson[] getStudentByTermWithFilter(String term) {
-        // GET {apiUrl}/searchAuthorDrupal.php?term={term}&filter={filter}
-        String filter = "&filter=" + uclouvainAuthorityConfiguration.getFilter();
-        String url = "/searchAuthorDrupal.php?term=" + term + filter;
+    public DialPerson[] getSuggestionByTermWithFilter(String term, String filterKey) {
+        String filter = "&filter=" + uclouvainAuthorityConfiguration.getFilterByKey(filterKey);
+        String cleanedTerm = this.encodeURL(term);
+        String url = "/searchAuthorDrupal.php?term=" + cleanedTerm + filter;
 
         DialPerson[] dialPerson = {};
 
@@ -44,11 +49,27 @@ public class DilbertClient implements UCLouvainAuthorAuthorityClient {
             HttpResponse<String> response = httpClient.get(url);
             dialPerson = new GenericResponse(response).extractJsonResponseDataToClass(null, DialPerson[].class);
         }
-        catch(IOException | InterruptedException e) {
-            System.err.println(e.getClass().getSimpleName() + "while fetching data :: " + e.getMessage());
+        catch(Exception e) {
+            logger.error(e.getClass().getSimpleName() + "while fetching data :: " + e.getMessage());
             e.printStackTrace(System.err);
         }
         return dialPerson;
+    }
+
+    // Utils
+    /**
+     * Encode URL to handle escaping characters like spaces.
+     * 
+     * @param url: The string url to encode.
+     */
+    private String encodeURL(String url) {
+        try {
+            return URLEncoder.encode(url, StandardCharsets.UTF_8.toString());
+        }
+        catch(UnsupportedEncodingException e) {
+            logger.error("Error while encodingURL ::" + e.getMessage());
+            throw new RuntimeException("Cannot encode: " + url);
+        }
     }
 
     // Setters && getters 
