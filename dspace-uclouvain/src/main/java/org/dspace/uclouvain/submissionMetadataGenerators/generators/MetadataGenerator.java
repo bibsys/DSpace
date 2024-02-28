e166726380f8e0f21c286e9858a05a07001d1fc4
MichaëlP <michael.pourbaix@uclouvain.be>
2024-02-28 10:18:43 +0100

    [ADD] feat(submission-action): Metadata generators on submit

---
+++ dspace-uclouvain/src/main/java/org/dspace/uclouvain/submissionMetadataGenerators/generators/MetadataGenerator.java
@@ -0,0 +1,10 @@
+package org.dspace.uclouvain.submissionMetadataGenerators.generators;
+
+import org.dspace.content.Item;
+import org.dspace.core.Context;
+import org.dspace.uclouvain.submissionMetadataGenerators.exceptions.GeneratorProcessException;
+
+public interface MetadataGenerator {
+    public String getGeneratorName();
+    public void process(Context ctx, Item item) throws GeneratorProcessException;
+}
