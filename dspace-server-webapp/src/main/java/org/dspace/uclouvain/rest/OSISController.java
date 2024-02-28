d209ade5ac28c2120b8b5e226e6898977408ee8c
MichaëlP <michael.pourbaix@uclouvain.be>
2024-02-28 10:18:43 +0100

    [ADD] feat(submission-action): Metadata generators on submit
    -> Added a more suitable type for masterthesis.degree.code field
    -> Removed the other masterthesis.degree.code field definition which was creating a duplicate in the form data
    -> Removed unused value-pair
    
    Co-authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-server-webapp/src/main/java/org/dspace/uclouvain/rest/OSISController.java
+++ dspace-server-webapp/src/main/java/org/dspace/uclouvain/rest/OSISController.java
@@ -82,12 +82,15 @@ public class OSISController {
         for (String fgs_id: fgs) {
             OSISStudentDegree[] osisStudentDegreeResponse = this.getAllStudentInfoByFGS(fgs_id);
             for (OSISStudentDegree studentDegree: osisStudentDegreeResponse) {
-                if (!studentDegree.isError() && (studentDegree.getSigleOffreRacine() != null)
-                    && studentDegree.getIntitOffreComplet().toLowerCase().contains(degreeTypeFilter.toLowerCase())
+                String degreeCode = studentDegree.getSigleOffreCompletN();
+                String degreeLabel = studentDegree.getIntitOffreComplet();
+                if (!studentDegree.isError() && degreeCode != null
+                    && !degreeCode.isBlank()
+                        && degreeLabel != null && !degreeLabel.isBlank()
+                        && degreeLabel.toLowerCase().contains(degreeTypeFilter.toLowerCase())
                 ) {
-                    String value = studentDegree.getSigleOffreRacine();
-                    String displayed = value + " - " + studentDegree.getIntitOffreComplet();
-                    selectFieldValues.addMetadataContentElementOption(value, displayed);
+                    String value = degreeCode + " - " + degreeLabel;
+                    selectFieldValues.addMetadataContentElementOption(value, value);
                 }
             }
         }
