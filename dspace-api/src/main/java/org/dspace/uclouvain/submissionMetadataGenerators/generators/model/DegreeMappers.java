2687dc5938f0c7ce71185059677bc34923a58c58
MichaëlP <michael.pourbaix@uclouvain.be>
2024-03-05 15:03:50 +0100

    [ADD] feat(submission): Adding root degree code, root degree label and faculty on submission
    Generating additionnal information (root code & label, faculty...) based on a mapping file and the degree.code of the thesis.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/submissionMetadataGenerators/generators/model/DegreeMappers.java
@@ -0,0 +1,17 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.submissionMetadataGenerators.generators.model;
+
+import java.util.HashMap;
+
+/**
+ * DegreeMappers: Represents a collection of DegreeMapper objects.
+ * Each one is linked to a specific degree code.
+ */
+public class DegreeMappers extends HashMap<String, DegreeMapper> {
+}
