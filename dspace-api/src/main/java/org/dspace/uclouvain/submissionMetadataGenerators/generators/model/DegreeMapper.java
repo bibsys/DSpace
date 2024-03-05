5bb55f48fa511da78dcf6cc0a2acf0f26df111f2
MichaëlP <michael.pourbaix@uclouvain.be>
2024-03-05 15:03:50 +0100

    [ADD] feat(submission): Adding root degree code, root degree label and faculty on submission
    Generating additionnal information (root code & label, faculty...) based on a mapping file and the degree.code of the thesis.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/submissionMetadataGenerators/generators/model/DegreeMapper.java
@@ -0,0 +1,49 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.submissionMetadataGenerators.generators.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DegreeMapper {
+    @JsonProperty("root-code")
+    private String rootDegreeCode;
+    @JsonProperty("root-label")
+    private String rootDegreeLabel;
+    @JsonProperty("faculty-code")
+    private String facultyCode;
+    @JsonProperty("faculty-name")
+    private String facultyName;
+
+    public String getRootDegreeCode() {
+        return rootDegreeCode;
+    }
+    public void setRootDegreeCode(String rootDegreeCode) {
+        this.rootDegreeCode = rootDegreeCode;
+    }
+
+    public String getRootDegreeLabel() {
+        return rootDegreeLabel;
+    }
+    public void setRootDegreeLabel(String rootDegreeLabel) {
+        this.rootDegreeLabel = rootDegreeLabel;
+    }
+
+    public String getFacultyCode() {
+        return facultyCode;
+    }
+    public void setFacultyCode(String facultyName) {
+        this.facultyCode = facultyName;
+    }
+
+    public String getFacultyName() {
+        return facultyName;
+    }
+    public void setFacultyName(String facultyName) {
+        this.facultyName = facultyName;
+    }
+}
