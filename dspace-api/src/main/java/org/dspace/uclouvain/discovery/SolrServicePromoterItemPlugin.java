6245dbaea8fbd7a071102cf46cb0f74e3ff61e35
MichaëlP <michael.pourbaix@uclouvain.be>
2024-03-19 16:42:01 +0100

    [ADD] feat(discovery): Discovery improves and changes
    * Modifying the `discovery.xml` config file to add new facet configuration to match the current DIAL.mem implementation.
    * Changed the base Solr Field prefix from `*_sort` to `*_keyword` since `*_sort` does not accept multiple values.
      This was bad since a thesis can have multiple degree codes.
    * Discovery config to allow promoter to see related archived items
    
    Auhtored-By: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/discovery/SolrServicePromoterItemPlugin.java
@@ -0,0 +1,32 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.discovery;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.dspace.core.Context;
+import org.dspace.discovery.DiscoverQuery;
+import org.dspace.discovery.SearchServiceException;
+import org.dspace.discovery.SolrServiceSearchPlugin;
+import org.dspace.eperson.EPerson;
+
+public class SolrServicePromoterItemPlugin implements SolrServiceSearchPlugin {
+    public static final String DISCOVER_PROMOTER_CONFIGURATION_NAME = "promoterConfiguration";
+
+    @Override
+    public void additionalSearchParameters(Context context, DiscoverQuery discoveryQuery, SolrQuery solrQuery)
+            throws SearchServiceException {
+        // Check if the current discovery configuration is for the promoter discovery
+        boolean isPromoterDiscovery = DISCOVER_PROMOTER_CONFIGURATION_NAME
+                .equals(discoveryQuery.getDiscoveryConfigurationName());
+        EPerson currentPerson = context.getCurrentUser();
+        if (currentPerson != null && isPromoterDiscovery) {
+            // Add a filter to the query that checks for the item field 'advisors.email'
+            solrQuery.addFilterQuery("advisors.email:" + currentPerson.getEmail());
+        }
+    }
+}
