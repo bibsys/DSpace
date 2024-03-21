a9779fedd87d8d66a65b63a56cd006730d3e9788
MichaëlP <michael.pourbaix@uclouvain.be>
2024-03-21 11:38:21 +0100

    [ADD] feat(indexing-facetting) Facetting && Indexing for the dcterms.accessRights metadata

--- dspace-uclouvain/src/main/java/org/dspace/uclouvain/core/GenericResponse.java
+++ dspace-uclouvain/src/main/java/org/dspace/uclouvain/core/GenericResponse.java
@@ -1,8 +1,6 @@
 package org.dspace.uclouvain.core;
 
 import java.io.IOException;
-import java.net.http.HttpResponse;
-
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,7 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 * Represent a basic Http Response from java.net.http lib
 * Used to access the JSON response
 */
-public class GenericResponse{
+public class GenericResponse {
 
     private String responseBody;
     // From Jackson, used to deserialize JSON to java 
@@ -35,13 +33,11 @@ public class GenericResponse{
      */
     public <T> T extractJsonResponseDataToClass(String targetKey, Class<T> clazz) throws IOException {
         this.objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-        
         JsonNode cleanedJson = this.objectMapper.readTree(responseBody);
         // If set, find the "targetKey" element's value
         if (targetKey != null) {
             cleanedJson = cleanedJson.findValue(targetKey);
         }
-
         // Convert JSON String into a java object
         return this.objectMapper.treeToValue(cleanedJson, clazz);
     }
