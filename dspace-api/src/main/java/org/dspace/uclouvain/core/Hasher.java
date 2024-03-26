f113954766b26f3cda4f964a3725fb7a89603d7c
MichaëlP <michael.pourbaix@uclouvain.be>
2024-03-26 11:48:37 +0100

    [UPD] feat(email-attestation): Attestation for promoters with links to bitstreams.
    Refactored `AttestationEmailAction` class in order to send emails to both authors and promoters.
    Promoters and authors receive both different mails. In the promoter's email, there are access urls to download bitstreams.
    
    Added a new controller which handles a new endpoint to download bitstream. This endpoint can be used by the promoter to retrieve a specific bitstream.
    A hash needs to be given in the request params. This hash must correspond to the hash of one of the promoter's email in order to download the bitstream's file.
    
    Authored-By: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/core/Hasher.java
@@ -0,0 +1,56 @@
+package org.dspace.uclouvain.core;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Basic class that can be used to hash data using a specific algorithm.
+ */
+public class Hasher {
+    private MessageDigest digest;
+
+    public Hasher(String algorithm, String encryptionKey) throws NoSuchAlgorithmException {
+        this.digest = MessageDigest.getInstance(algorithm);
+        this.digest.update(encryptionKey.getBytes());
+    }
+
+    public Hasher(String algorithm) throws NoSuchAlgorithmException {
+        this.digest = MessageDigest.getInstance(algorithm);
+    }
+
+    /**
+     * Process the hash of a given string.
+     * The result depends on the configured algorithm.
+     * @param data: The data to hash.
+     * @return: The hash of the data.
+     */
+    public byte[] processHash(String data){
+        return digest.digest(data.getBytes());
+    }
+
+    /**
+     * Process the hash of a given string and return it as a string.
+     * The result depends on the configured algorithm.
+     * @param data: The data to hash.
+     * @return: The hash of the data as a string.
+     */
+    public String processHashAsString(String data){
+        byte[] hash =  this.processHash(data);
+        return this.bytesToHexString(hash);
+
+    }
+
+    /**
+     * Converts a given byte array to an Hexadecimal string notation.
+     * For each byte in the array, append the hexadecimal representation.
+     * @param bytes: An array of bytes that need to be converted.
+     * @return: A string containing the full hexadecimal notation for the given array.
+     */
+    private String bytesToHexString(byte[] bytes){
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+    }
+}
