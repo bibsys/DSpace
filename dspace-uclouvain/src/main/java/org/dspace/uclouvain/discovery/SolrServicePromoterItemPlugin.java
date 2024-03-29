c6c3ef7d98f33671ab9ad72379a837f1f2260d0d
MichaëlP <michael.pourbaix@uclouvain.be>
2024-03-29 14:32:42 +0100

    [ADD] feat(promoter-discovery-config): Discovery config to allow promoter to see related archived items
    New configuration in `discovery.xml` that allows a promoter to log in and to see all the related submission in the 'mydspace' page.

---
+++ dspace-uclouvain/src/main/java/org/dspace/uclouvain/discovery/SolrServicePromoterItemPlugin.java
@@ -0,0 +1,24 @@
+package org.dspace.uclouvain.discovery;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.dspace.core.Context;
+import org.dspace.discovery.DiscoverQuery;
+import org.dspace.discovery.SearchServiceException;
+import org.dspace.discovery.SolrServiceSearchPlugin;
+import org.dspace.eperson.EPerson;
+
+public class SolrServicePromoterItemPlugin implements SolrServiceSearchPlugin {
+    public static final String DISCOVER_PROMOTER_CONFIGURATION_NAME = "promoterConfiguration";
+
+    @Override
+    public void additionalSearchParameters(Context context, DiscoverQuery discoveryQuery, SolrQuery solrQuery) throws SearchServiceException {
+        // Check if the current discovery configuration is for the promoter discovery
+        boolean isPromoterDiscovery = DISCOVER_PROMOTER_CONFIGURATION_NAME.equals(discoveryQuery.getDiscoveryConfigurationName());
+
+        EPerson currentPerson = context.getCurrentUser();
+        if (currentPerson != null && isPromoterDiscovery) {
+            // Add a filter to the query that checks for the item field 'advisors.email'
+            solrQuery.addFilterQuery("advisors.email:" + currentPerson.getEmail());
+        }
+    }
+}
