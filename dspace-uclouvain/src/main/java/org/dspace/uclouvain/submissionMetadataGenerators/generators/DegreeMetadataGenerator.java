a81bc8f94a4bde0526dd9c3a780fc24baaef5d83
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-04-08 14:00:46 +0200

    [UPD] feat(metadata-generation): Update degree code/label generation
    * "students/degree" REST API response updated to include only "degreeCode" into returned "value" (displayed data still contains code and label).
    * It's now no more required to extract "degree label" from "degree code" field; The Angular frontend must send correct data throught API.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- dspace-uclouvain/src/main/java/org/dspace/uclouvain/submissionMetadataGenerators/generators/DegreeMetadataGenerator.java
+++ dspace-uclouvain/src/main/java/org/dspace/uclouvain/submissionMetadataGenerators/generators/DegreeMetadataGenerator.java
@@ -1,8 +1,6 @@
 package org.dspace.uclouvain.submissionMetadataGenerators.generators;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,15 +11,13 @@ import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.dspace.uclouvain.core.model.MetadataField;
 import org.dspace.uclouvain.submissionMetadataGenerators.exceptions.GeneratorProcessException;
-import org.dspace.uclouvain.submissionMetadataGenerators.exceptions.MalformedDegreeCodeException;
 import org.dspace.uclouvain.submissionMetadataGenerators.generators.model.DegreeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Generator in charge of generating degree metadata.
- * First it will split the degree code in two parts (degree code and label) and replace the degree code and label.
- * Then it will use the degree mappers service to retrieve additional information based on the degree code.
- * Finally it will add the additional information to the item.
+ * Use degree mappers service to retrieve additional information based on the publication degree codes and store these
+ * additional information into the item metadata.
  */
 public class DegreeMetadataGenerator implements MetadataGenerator {
 
@@ -49,59 +45,36 @@ public class DegreeMetadataGenerator implements MetadataGenerator {
         return this.NAME;
     }
 
+
     @Override
     public void process(Context ctx, Item item) throws GeneratorProcessException {
         try {
             ItemService currentItemService = item.getItemService();
-            // 1) Clear previous metadata Values
+            // 1) Clear previous metadata values (rootDegree, faculty, ...)
             this.clearPreviousMetadata(ctx, item, currentItemService);
 
-            // 2) Retrieve a list of distinct degree codes and labels splitted by '-'
+            // 2) Get information based on the distinct retrieved degree codes and the degree mappers file
             List<MetadataValue> currentDegreeCodes = currentItemService.getMetadata(
-                item,
-                this.degreeCodeField.getSchema(),
+                item, this.degreeCodeField.getSchema(),
                 this.degreeCodeField.getElement(),
                 this.degreeCodeField.getQualifier(),
                 null
             );
-            // Generate code and labels in a list of pairs: [['code1', 'label1'], ['code2', 'label2'], ['code3', 'label3']]
-            List<List<String>> codeAndLabels = this.generateCodeAndLabels(currentDegreeCodes);
-            // Loop over the pairs and add the corresponding metadata to the item
-            for (Integer elementIndex = 0; elementIndex < codeAndLabels.size(); elementIndex++) {
-                List<String> values = codeAndLabels.get(elementIndex);
-                // Replace degree code
-                currentItemService.replaceSecuredMetadata(
-                    ctx, item,
-                    this.degreeCodeField.getSchema(),
-                    this.degreeCodeField.getElement(),
-                    this.degreeCodeField.getQualifier(),
-                    null, values.get(0), null, -1, elementIndex, 0
-                );
-                // Add degree label
-                currentItemService.addMetadata(
-                    ctx, item,
-                    this.degreeLabelField.getSchema(),
-                    this.degreeLabelField.getElement(),
-                    this.degreeLabelField.getQualifier(),
-                    null, values.get(1), null, -1, 0
-                );
-            }
-
-            // 3) Generate new information based on the distinct retrieved degree codes and the degree mappers file
-            List<String> distinctCodes = codeAndLabels.stream().map(x -> x.get(0)).distinct().collect(Collectors.toList());
+            List<String> distinctCodes = currentDegreeCodes
+                .stream()
+                .map(MetadataValue::getValue)
+                .distinct()
+                .collect(Collectors.toList());
             List<DegreeMapper> mappings = this.degreeMappersService.getDegreeMappersForDegreeCodes(distinctCodes);
             // For each mapping found, add additional info to the item
             for (DegreeMapper mapping: mappings) {
                 generateAdditionalMetadataFromMapper(ctx, item, currentItemService, mapping);
             }
-
-            // 4) Update the item with the additional metadata
+            // 3) Update the item with the additional metadata
             currentItemService.update(ctx, item);
 
         } catch (SQLException e) {
             throw new GeneratorProcessException("An error occurred while replacing degree metadata", e);
-        } catch (MalformedDegreeCodeException e) {
-            throw new GeneratorProcessException("Could not generate degree code and label due to malformed degree code", e);
         } catch (AuthorizeException e) {
             throw new GeneratorProcessException("Not authorized to update the item", e);
         }
@@ -114,25 +87,6 @@ public class DegreeMetadataGenerator implements MetadataGenerator {
     }
 
     /**
-     * Generate a list of code and label pairs from a list of metadata values.
-     * @param metadataValues: List of metadata values of the ´degree.code´ field.
-     * @return List of code and label pairs.
-     * @throws MalformedDegreeCodeException
-     */
-    private List<List<String>> generateCodeAndLabels(List<MetadataValue> metadataValues) throws MalformedDegreeCodeException {
-        List<List<String>> choppedList = new ArrayList<List<String>>();
-        for (MetadataValue mv: metadataValues) {
-            List<String> codeAndLabel = Arrays.asList(mv.getValue().split("-"));
-            if (codeAndLabel.size() != 2) {
-                throw new MalformedDegreeCodeException("Malformed degree code and label combination: " + mv.getValue());
-            }
-            codeAndLabel.replaceAll(String::trim);
-            choppedList.add(codeAndLabel);
-        }
-        return choppedList;
-    }
-
-    /**
      * Clear previous metadata values for the degree code, degree label, root degree code, root degree label, faculty code and faculty name.
      * @param ctx: The DSpace context.
      * @param item: The item to update.
@@ -140,13 +94,6 @@ public class DegreeMetadataGenerator implements MetadataGenerator {
      * @throws SQLException
      */
     private void clearPreviousMetadata(Context ctx, Item item, ItemService itemService) throws SQLException {
-        itemService.clearMetadata(
-            ctx, item,
-            this.degreeLabelField.getSchema(),
-            this.degreeLabelField.getElement(),
-            this.degreeLabelField.getQualifier(),
-            null
-        );
         itemService.clearMetadata(
             ctx, item,
             this.rootDegreeCodeField.getSchema(),
@@ -185,39 +132,41 @@ public class DegreeMetadataGenerator implements MetadataGenerator {
      * @throws SQLException
      */
     private void generateAdditionalMetadataFromMapper(Context ctx, Item item, ItemService itemService, DegreeMapper mapping) throws SQLException {
-        itemService.addMetadata(
-            ctx, item,
-            this.rootDegreeCodeField.getSchema(),
-            this.rootDegreeCodeField.getElement(),
-            this.rootDegreeCodeField.getQualifier(),
-            null, mapping.getRootDegreeCode()
-        );
-        itemService.addMetadata(
-            ctx, item,
-            this.rootDegreeLabelField.getSchema(),
-            this.rootDegreeLabelField.getElement(),
-            this.rootDegreeLabelField.getQualifier(),
-            null , mapping.getRootDegreeLabel()
-        );
-        itemService.addMetadata(
-            ctx, item,
-            this.facultyCodeField.getSchema(),
-            this.facultyCodeField.getElement(),
-            this.facultyCodeField.getQualifier(),
-            null, mapping.getFacultyCode()
-        );
-        itemService.addMetadata(
-            ctx, item,
-            this.facultyNameField.getSchema(),
-            this.facultyNameField.getElement(),
-            this.facultyNameField.getQualifier(),
-            null, mapping.getFacultyName()
-        );
+        if (mapping.getRootDegreeCode() != null && mapping.getRootDegreeLabel() != null) {
+            itemService.addMetadata(
+                ctx, item,
+                this.rootDegreeCodeField.getSchema(),
+                this.rootDegreeCodeField.getElement(),
+                this.rootDegreeCodeField.getQualifier(),
+                null, mapping.getRootDegreeCode()
+            );
+            itemService.addMetadata(
+                ctx, item,
+                this.rootDegreeLabelField.getSchema(),
+                this.rootDegreeLabelField.getElement(),
+                this.rootDegreeLabelField.getQualifier(),
+                null, mapping.getRootDegreeLabel()
+            );
+        }
+        if (mapping.getFacultyCode() != null && mapping.getFacultyName() != null) {
+            itemService.addMetadata(
+                ctx, item,
+                this.facultyCodeField.getSchema(),
+                this.facultyCodeField.getElement(),
+                this.facultyCodeField.getQualifier(),
+                null, mapping.getFacultyCode()
+            );
+            itemService.addMetadata(
+                ctx, item,
+                this.facultyNameField.getSchema(),
+                this.facultyNameField.getElement(),
+                this.facultyNameField.getQualifier(),
+                null, mapping.getFacultyName()
+            );
+        }
     }
 
-    // -------------------
-    // Getters && Setters
-    // -------------------
+    // Getters && Setters =====================================================
 
     public List<String> getAcceptedEntityTypes(){
         return this.acceptedEntityTypes;
