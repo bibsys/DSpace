e80c6b23aba305ed2f4adc0089dc85e8b141d887
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-04-08 14:00:46 +0200

    [UPD] feat(metadata-generation): Update degree code/label generation
    * "students/degree" REST API response updated to include only "degreeCode" into returned "value" (displayed data still contains code and label).
    * It's now no more required to extract "degree label" from "degree code" field; The Angular frontend must send correct data throught API.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- dspace-uclouvain/src/main/java/org/dspace/uclouvain/submissionMetadataGenerators/generators/MetadataGenerator.java
+++ dspace-uclouvain/src/main/java/org/dspace/uclouvain/submissionMetadataGenerators/generators/MetadataGenerator.java
@@ -5,7 +5,7 @@ import org.dspace.core.Context;
 import org.dspace.uclouvain.submissionMetadataGenerators.exceptions.GeneratorProcessException;
 
 public interface MetadataGenerator {
-    public String getGeneratorName();
-    public void process(Context ctx, Item item) throws GeneratorProcessException;
-    public Boolean canBeProcessed(Context ctx, Item item);
+    String getGeneratorName();
+    void process(Context ctx, Item item) throws GeneratorProcessException;
+    Boolean canBeProcessed(Context ctx, Item item);
 }
