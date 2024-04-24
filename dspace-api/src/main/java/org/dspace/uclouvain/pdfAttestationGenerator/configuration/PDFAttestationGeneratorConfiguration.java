79fc0d9e8db643efc598e1bb8e8fd9414e97a5a4
MichaëlP <michael.pourbaix@uclouvain.be>
2024-04-24 11:22:03 +0200

    [UPD] feat(pdf-attestation): Refactoring to use bean + authorization feature modification
    1. Removed the xml config file to use bean instead. It leads to a simpler configuration for the functionnality.
    2. Changed the implementation for the 'CanDownloadPDFAttestationFeature' class. Now the permissions is granted if:
    	- the item can be handled by a handler AND if(
    	     - the user is an admin OR
    	     - the user is the submitter of the item OR
    	     - the user is a manager
    	)
    3. Lot of refactoring in different class files.
    
    Authored-By: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/pdfAttestationGenerator/configuration/PDFAttestationGeneratorConfiguration.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/pdfAttestationGenerator/configuration/PDFAttestationGeneratorConfiguration.java
@@ -1,60 +1,48 @@
 package org.dspace.uclouvain.pdfAttestationGenerator.configuration;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import org.dspace.services.factory.DSpaceServicesFactory;
 import org.dspace.uclouvain.pdfAttestationGenerator.model.Handler;
-import org.dspace.uclouvain.pdfAttestationGenerator.model.Handlers;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.security.NoTypePermission;
-import com.thoughtworks.xstream.security.NullPermission;
-import com.thoughtworks.xstream.security.PrimitiveTypePermission;
 
 public class PDFAttestationGeneratorConfiguration {
 
-    private Handlers handlersConfiguration;
-
-    @Autowired
-    private String source = DSpaceServicesFactory.getInstance().getConfigurationService().getProperty("dspace.dir");
+    private List<Handler> handlersConfiguration;
 
     /** 
-     * Loads the xml configuration file in a java object 
+     * Search for a specific configuration based on the itemType.
      * 
-     * @param xmlConfigPath: The path to the configuration file starting from "dspace.dir"
-     */
-    public PDFAttestationGeneratorConfiguration(String xmlConfigPath) throws FileNotFoundException {
-        XStream xStream = new XStream();
-        File xmlConfig = new File(this.source + xmlConfigPath);
-        if (xmlConfig.exists()) {
-            // Adding permissions to allow access to classes
-            xStream.addPermission(NoTypePermission.NONE);
-            xStream.addPermission(NullPermission.NULL);
-            xStream.addPermission(PrimitiveTypePermission.PRIMITIVES);
-            xStream.allowTypesByWildcard(new String[] {
-                "org.dspace.uclouvain.pdfAttestationGenerator.model.Handler",
-                "org.dspace.uclouvain.pdfAttestationGenerator.model.Handlers",            
-            });
-            xStream.processAnnotations(Handlers.class);
-            this.handlersConfiguration = (Handlers)xStream.fromXML(xmlConfig);
-        }
-        else {
-            throw new FileNotFoundException("Could not find the PDF attestation generator configuration file");
-        }
-    }
-
-    /** 
-     * Search for a specific configuration based on the itemType
-     * 
-     * @param itemType: The type of the item to search for the config
-     * @return Handler or null if not found
+     * @param itemType: The type of the item to search for the config.
+     * @return Handler object or null if not found.
      */
     public Handler getConfigForItemType(String itemType) {
-        for (Handler handler: this.handlersConfiguration.handlers) {
+        for (Handler handler: this.handlersConfiguration) {
             if (handler.itemType.equals(itemType)) return handler;
         }
         return null;
+    }
+
+    /**
+     * Get a list of all handled item types for PDF attestation generation.
+     * 
+     * @return List of all handled item types.
+     */
+    public List<String> getAllHandledTypes() {
+        return this.handlersConfiguration
+            .stream()
+            // Could maybe be changed to 'Handler::getItemType' ??? Not sure
+            .map(handler -> handler.getItemType())
+            .distinct()
+            .collect(Collectors.toList());
+    }
+
+    // GETTERS && SETTERS
+
+    public List<Handler> getHandlersConfiguration() {
+        return this.handlersConfiguration;
+    }
+
+    public void setHandlersConfiguration(List<Handler> handlersConfiguration) {
+        this.handlersConfiguration = handlersConfiguration;
     }
 }
