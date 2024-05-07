14444e4c6cb4bdb1167447e9e56709aa424ff431
MichaëlP <michael.pourbaix@uclouvain.be>
2024-05-07 08:59:33 +0200

    [ADD] feat(API): Resource policies API for bitstreams && items
    New API that can be used to retrieve resource policies information about a given bitstream or item.
    One endpoint to handle the 'item' type and one for the 'bitstream' type.
    Also added a service (to group similar code from 'AccessTypeMetadataGenerator') called 'ResourcePolicyUtilService'.
    
    Authored-By: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/core/model/ResourcePolicyRestResponse.java
@@ -0,0 +1,43 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.core.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.dspace.authorize.ResourcePolicy;
+
+/**
+ * This is a class that returns resource policy information for a REST API response.
+ */
+public class ResourcePolicyRestResponse {
+    public List<ResourcePolicyRestModel> restPolicies = new ArrayList<>();
+    public ResourcePolicyRestModel masterPolicy;
+
+    /**
+     * Main constructor for the ResourcePolicyRestResponse class.
+     * It filters the policies by type and creates a list of ResourcePolicyRestModel objects.
+     *
+     * @param policies The list of resource policies to use to instantiate the classes.
+     * @param type A type to filter the policies by.
+     */
+    public ResourcePolicyRestResponse(List<ResourcePolicy> policies, String type) {
+        this(policies.stream().filter(p -> p.getRpType().equals(type)).collect(Collectors.toList()));
+    }
+
+    /**
+     * Main constructor for the ResourcePolicyRestResponse class.
+     * It creates a list of ResourcePolicyRestModel objects without any filtering.
+     *
+     * @param policies The list of resource policies to use to instantiate the classes.
+     */
+    public ResourcePolicyRestResponse(List<ResourcePolicy> policies) {
+        policies.forEach(p -> restPolicies.add(new ResourcePolicyRestModel(p)));
+    }
+}
