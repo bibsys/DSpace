eb4a3972e51a06d2832f23c00b63f52282cf699e
MichaëlP <michael.pourbaix@uclouvain.be>
2024-06-13 11:57:18 +0200

    [ADD] feat(bitstream-download-url): REST endpoint for bitstream download url.
    Created a new REST repository link that allows to access the bitstream download URL for managers && promoters.
    This link is only valid when the item is in validation (workflow) stage.
    
    @Authored-By: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/pdfAttestationGenerator/AttestationAuthorizationService.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/pdfAttestationGenerator/AttestationAuthorizationService.java
@@ -3,18 +3,13 @@ package org.dspace.uclouvain.pdfAttestationGenerator;
 import java.sql.SQLException;
 
 import org.dspace.authorize.service.AuthorizeService;
-import org.dspace.content.Collection;
 import org.dspace.content.Item;
 import org.dspace.content.service.ItemService;
-import org.dspace.content.service.WorkspaceItemService;
 import org.dspace.core.Context;
 import org.dspace.eperson.EPerson;
-import org.dspace.eperson.Group;
+import org.dspace.uclouvain.core.utils.AuthorizationUtils;
+import org.dspace.uclouvain.core.utils.ItemUtils;
 import org.dspace.uclouvain.pdfAttestationGenerator.configuration.PDFAttestationGeneratorConfiguration;
-import org.dspace.xmlworkflow.storedcomponents.CollectionRole;
-import org.dspace.xmlworkflow.storedcomponents.XmlWorkflowItem;
-import org.dspace.xmlworkflow.storedcomponents.service.CollectionRoleService;
-import org.dspace.xmlworkflow.storedcomponents.service.XmlWorkflowItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 // Service to check permission for attestation generation and download.
@@ -27,16 +22,13 @@ public class AttestationAuthorizationService {
     private ItemService itemService;
 
     @Autowired
-    private XmlWorkflowItemService xmlWorkflowItemService;
-
-    @Autowired
-    private CollectionRoleService collectionRoleService;
-
-    @Autowired
-    private WorkspaceItemService workspaceItemService;
-
-    @Autowired
     private PDFAttestationGeneratorConfiguration pdfAttestationGeneratorConfiguration;
+
+    @Autowired
+    private AuthorizationUtils authUtils;
+
+    @Autowired
+    private ItemUtils itemUtils;
 
     /**
      * Check if the item is valid for the current attestation configuration.
@@ -45,13 +37,13 @@ public class AttestationAuthorizationService {
      * @return True if the item is valid for at least one of the configured attestation.
      * @throws SQLException
      */
-    public Boolean isItemValidForAttestation(Item item, Context context) throws SQLException {
+    public boolean isItemValidForAttestation(Item item, Context context) throws SQLException {
         // Check that the item is not in the workspace
-        return workspaceItemService.findByItem(context, item) == null 
+        return !this.itemUtils.isWorkspace(context, item)
             && this.pdfAttestationGeneratorConfiguration
                 .getAllHandledTypes()
                 .contains(
-                    itemService.getMetadataFirstValue(
+                    this.itemService.getMetadataFirstValue(
                         item, 
                         "dspace", 
                         "entity", 
@@ -74,43 +66,11 @@ public class AttestationAuthorizationService {
      * @return True if the user is validates one of the above conditions.
      * @throws SQLException
      */
-    public Boolean isUserAuthorized(Item dsItem, Context ctx) throws SQLException {
+    public boolean isUserAuthorized(Item dsItem, Context ctx) throws SQLException {
         EPerson currentUser = ctx.getCurrentUser();
         if (currentUser == null) return false; 
-        return (authorizeService.isAdmin(ctx, dsItem))
+        return (this.authorizeService.isAdmin(ctx, dsItem))
             || (dsItem.getSubmitter() == currentUser)
-            || (this.isManagerOfItem(dsItem, currentUser, ctx));
-    }
-
-    /**
-     * Checks if the current user is a manager of the item's collection.
-     * Retrieve the group for the collection that corresponds to the reviewer role and check if the current user is a member.
-     * @param item: The item to check permission for.
-     * @param currentUser: The current logged user.
-     * @param ctx: The current DSpace context.
-     * @return True if the user is a manager of the item's collection else false.
-     * @throws SQLException
-     */
-    public Boolean isManagerOfItem(Item item, EPerson currentUser, Context ctx) throws SQLException {
-        Collection collection = item.getOwningCollection();
-        if (collection == null) {
-            // Check if the item is in the workflow, if it is we need to use the XmlWorkflowItem to retrieve the owning collection.
-            XmlWorkflowItem xmlWorkflowItem = xmlWorkflowItemService.findByItem(ctx, item);
-            if (xmlWorkflowItem != null) {
-                collection = xmlWorkflowItem.getCollection();
-            };
-        }
-
-        // Retrieve all the roles created for the item's collection.
-        for (CollectionRole role : collectionRoleService.findByCollection(ctx, collection)) {
-            if (role.getRoleId().equals(CollectionRoleService.LEGACY_WORKFLOW_STEP1_NAME)) {
-                for (Group group: role.getGroup().getMemberGroups()){
-                    if (group.getMembers().contains(currentUser)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+            || (this.authUtils.isManagerOfItem(ctx, dsItem, currentUser));
     }
 }
---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/BitstreamDirectDownloadURLPermissionEvaluatorPlugin.java
@@ -0,0 +1,112 @@
+package org.dspace.app.rest.security;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import org.dspace.services.model.Request;
+import org.dspace.uclouvain.core.utils.AuthorizationUtils;
+import org.dspace.uclouvain.core.utils.ItemUtils;
+import org.dspace.uclouvain.rest.BitstreamDirectDownloadRestController;
+import org.dspace.web.ContextUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dspace.authorize.service.AuthorizeService;
+import org.dspace.content.Bitstream;
+import org.dspace.content.Item;
+import org.dspace.content.service.BitstreamService;
+import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
+import org.dspace.services.RequestService;
+
+/** 
+ * EvaluatorPlugin which deals with the permissions for the bitstream download URL generation.
+ * 
+ * @Author: Michaël Pourbaix <michael.pourbaix@uclouvain.be>
+ */ 
+@Component
+public class BitstreamDirectDownloadURLPermissionEvaluatorPlugin extends RestObjectPermissionEvaluatorPlugin {
+
+    private final static String DOWNLOAD_URL_PERMISSION = "DOWNLOAD_URL";
+
+    @Autowired
+    private RequestService requestService;
+
+    @Autowired
+    private BitstreamService bitstreamService;
+
+    @Autowired
+    private ItemUtils itemUtils;
+
+    @Autowired
+    private AuthorizationUtils authUtils; 
+
+    @Autowired
+    private AuthorizeService authorizeService;
+
+    private Logger logger = LogManager.getLogger(BitstreamDirectDownloadRestController.class);
+
+    /**
+     * Handles the permission evaluation for the download URL generation.
+     * Example of call to this method with the '@PreAuthorize' decorator: `@PreAuthorize("hasPermission(#bitstreamId, 'BITSTREAM', 'DOWNLOAD_URL')")`.
+     */
+    @Override
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
+        if (permission.toString().equalsIgnoreCase(DOWNLOAD_URL_PERMISSION) && targetId != null && targetType.equals("BITSTREAM")) {
+            Request request = this.requestService.getCurrentRequest();
+            Context context = ContextUtil.obtainContext(request.getHttpServletRequest());
+
+            EPerson currentUser = context.getCurrentUser();
+            
+            // Check if user is logged in
+            if (currentUser != null) {
+                try {
+                    Bitstream targetBitstream = this.bitstreamService.find(context, UUID.fromString(targetId.toString()));
+                    Item item = this.itemUtils.getItemFromBitstream(context, targetBitstream);
+                    if (item != null) {
+                        return this.isAuthorized(context, item, currentUser);
+                    }
+                } catch (Exception e) {
+                    logger.error("Error in custom BitstreamDownloadURLPermissionEvaluatorPlugin: " + e.getMessage());
+                    return false;
+                }
+            }
+        }
+        System.out.println("Targetid was probably null: " + targetId + " and targetType: " + targetType + " and permission: " + permission);
+        return false;
+    }
+
+    @Override
+    public boolean hasDSpacePermission(Authentication authentication, Serializable targetId, String targetType, DSpaceRestPermission restPermission) {
+        return false;
+    }
+
+    // Checks if with the current context (item && user) we can generate a download URL.
+    private boolean isAuthorized(Context context, Item item, EPerson currentUser) {
+        try {
+            return this.itemUtils.isWorkflow(context, item) && this.isUserAuthorized(context, item, currentUser); 
+        } catch (SQLException e) {
+            logger.warn("An error occurred while checking the authorization for the download URL generation with item UUID: " + item.getID() + " && user: " + currentUser.getEmail() + "; " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Checks if the user has authorization to access the download URL.
+     * The user has authorization if:
+     * - User is an admin -OR-
+     * - User is a manager for the collection
+     * @param context: The current Dspace Context
+     * @param item: The item to check for authorization.
+     * @param currentUser: The current logged user.
+     * @return True if the user meets one of the requirements listed above, false otherwise.
+     * @throws SQLException
+     */
+    private boolean isUserAuthorized(Context context, Item item, EPerson currentUser) throws SQLException {
+        return this.authorizeService.isAdmin(context, currentUser) ||
+            this.authUtils.isManagerOfItem(context, item, currentUser);
+    }
+}
