76d93a0fc1cbbdadb1d08ebeb5e5b1318e6cac92
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-07-09 15:55:44 +0200

    [ADD] feat(cli): Additional CLI command & Setup script adaptation
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- dspace-uclouvain/src/main/java/org/dspace/uclouvain/administer/UtilityCLITool.java
+++ dspace-uclouvain/src/main/java/org/dspace/uclouvain/administer/UtilityCLITool.java
@@ -3,10 +3,42 @@ package org.dspace.uclouvain.administer;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
+import org.dspace.eperson.factory.EPersonServiceFactory;
+import org.dspace.eperson.service.EPersonService;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UtilityCLITool {
+
+    /**
+     * Find an ePerson based either on email, either on UUID (aka. NetID)
+     *
+     * @param userID: the user's identifier
+     * @return the corresponding ePerson
+     * @throws IllegalStateException if a database exception occurred or no ePerson is found.
+     */
+    public static EPerson findUser(Context ctx, String userID) throws IllegalStateException {
+        EPersonService service = EPersonServiceFactory.getInstance().getEPersonService();
+        if (userID == null || userID.isEmpty()) {
+            throw new IllegalStateException("user ID must be specified");
+        }
+        try {
+            EPerson eperson = service.findByEmail(ctx, userID);
+            if (eperson != null) {
+                return eperson;
+            }
+            eperson = service.findByNetid(ctx, userID);
+            if (eperson != null) {
+                return eperson;
+            }
+        } catch(SQLException e) {
+            throw new IllegalStateException("User not found :: "+e.getMessage());
+        }
+        throw new IllegalStateException("User not found");
+    }
 
     /**
      * Allows to combine multiple CLI options group into a single options set.
@@ -25,6 +57,13 @@ public class UtilityCLITool {
         return opts;
     }
 
+    /**
+     * Display the usage of a command line with all possible arguments.
+     *
+     * @param cls: the CLI class to manage
+     * @param description: a human-readable description for the CLI.
+     * @param optionsGroupList: All options available for the CLI.
+     */
     public static void usage(Class<?> cls, String description, List<Options> optionsGroupList) {
         String header = "\n" + description + "\n\n";
         String footer = "\n";
