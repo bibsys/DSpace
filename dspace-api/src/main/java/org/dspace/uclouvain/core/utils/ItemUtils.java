ac0a549c829129c9c960f72453be5b6fc78b2517
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-07-10 13:50:38 +0200

    [ADD] feat(general): Adds some item utils methods
    Co-authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/core/utils/ItemUtils.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/core/utils/ItemUtils.java
@@ -7,32 +7,53 @@
  */
 package org.dspace.uclouvain.core.utils;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
+import org.dspace.content.Collection;
+import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
+import org.dspace.content.WorkspaceItem;
+import org.dspace.content.service.BitstreamService;
+import org.dspace.content.service.WorkspaceItemService;
+import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
+import org.dspace.eperson.Group;
 import org.dspace.services.factory.DSpaceServicesFactory;
+import org.dspace.xmlworkflow.storedcomponents.CollectionRole;
+import org.dspace.xmlworkflow.storedcomponents.XmlWorkflowItem;
+import org.dspace.xmlworkflow.storedcomponents.service.CollectionRoleService;
+import org.dspace.xmlworkflow.storedcomponents.service.XmlWorkflowItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /** 
-* Set of util methods for an `Item` object.
+ * Set of util methods for an `Item` object.
+ *
+ * @Author: Michaël Pourbaix <michael.pourbaix@uclouvain.be>
 */
 public class ItemUtils {
 
-    private ItemUtils() {
-        throw new UnsupportedOperationException();
-    }
+    @Autowired
+    private BitstreamService bitstreamService;
+    @Autowired
+    private XmlWorkflowItemService xmlWorkflowItemService;
+    @Autowired
+    private WorkspaceItemService workspaceItemService;
+    @Autowired
+    private CollectionRoleService collectionRoleService;
 
     /**
-    * This method is used to extract the attached files from an item.
+    * This method is used to extract the files's bit stream from an item.
     * 
     * @param item The item to extract files from.
     * @return The list of bit streams for the given item.
     */
     public static List<Bitstream> extractItemFiles(Item item) {
-        // Configuration which indicates the bundles to use.
+        // Configuration which gives the bundles names to use.
         List<String> acceptedBundles = Arrays.asList(
             DSpaceServicesFactory.getInstance().getConfigurationService()
                     .getArrayProperty("uclouvain.resource_policy.accepted_bundles")
@@ -44,5 +65,98 @@ public class ItemUtils {
             }
         }
         return bitstreams;
+    }
+
+    /**
+     * Returns the list of all valid manager for a given item.
+     * @param context The current DSpace context.
+     * @param item The item to get the managers from.
+     * @return The list of all valid managers for the given item.
+     * @throws SQLException
+     */
+    public List<EPerson> getManagersOfItem(Context context, Item item) throws SQLException {
+        List<EPerson> managers = new ArrayList<>();
+        Collection collection = item.getOwningCollection();
+
+        if (collection == null) {
+            // Check if the item is in the workflow, if it is we need to use the XmlWorkflowItem to retrieve the
+            // owning collection.
+            XmlWorkflowItem xmlWorkflowItem = xmlWorkflowItemService.findByItem(context, item);
+            if (xmlWorkflowItem != null) {
+                collection = xmlWorkflowItem.getCollection();
+            }
+        }
+
+        // Retrieve all the roles created for the item's collection.
+        for (CollectionRole role : collectionRoleService.findByCollection(context, collection)) {
+            if (role.getRoleId().equals(CollectionRoleService.LEGACY_WORKFLOW_STEP1_NAME)) {
+                for (Group group: role.getGroup().getMemberGroups()) {
+                    managers.addAll(group.getMembers());
+                }
+            }
+        }
+        return managers;
+    }
+
+    /**
+    * This method allows to get the root item of a bitstream.
+    *
+    * @param context The current Dspace context.
+    * @param bitstream The bitstream to get the item from.
+    * @return The item that contains the given bitstream or null if none.
+    * @throws SQLException
+    */
+    public Item getItemFromBitstream(Context context, Bitstream bitstream) throws SQLException {
+        DSpaceObject dso = bitstreamService.getParentObject(context, bitstream);
+        return (dso instanceof Item)
+            ? (Item) dso
+            : null;
+    }
+
+    /**
+     * Get the main collection for a given item.
+     * Depending on item life-cycle, the item's main collection should be retrieved differently.
+     *
+     * @param context The DSpace context.
+     * @param item    The DSpace Item to analyze.
+     * @return The main collection to which the item belongs. Returns null if not found.
+     */
+    public Collection getMainCollection(Context context, Item item) {
+        try {
+            WorkspaceItem wsItem = workspaceItemService.findByItem(context, item);
+            if (wsItem != null) {
+                return wsItem.getCollection();
+            }
+            XmlWorkflowItem wfItem = xmlWorkflowItemService.findByItem(context, item);
+            if (wfItem != null) {
+                return wfItem.getCollection();
+            }
+            return item.getOwningCollection();
+        } catch (SQLException sqe) {
+            return null;
+        }
+    }
+
+    /**
+     * Checks if an item is in workflow validation.
+     * @param context The current DSpace context.
+     * @param item The item to check.
+     * @return True if the item is in workflow false otherwise.
+     * @throws SQLException
+     *
+     */
+    public boolean isWorkflow(Context context, Item item) throws SQLException {
+        return xmlWorkflowItemService.findByItem(context, item) != null;
+    }
+
+    /**
+     * Checks if the item is in the workspace.
+     * @param context The current DSpace context.
+     * @param item The item to check.
+     * @return True if the item is in workspace false otherwise.
+     * @throws SQLException
+     */
+    public boolean isWorkspace(Context context, Item item) throws SQLException {
+        return workspaceItemService.findByItem(context, item) != null;
     }
 }
