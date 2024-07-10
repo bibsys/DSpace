57b6644efcf8023901c6575267595bccd487b8ba
Renaud Michotte <renaud.michotte@uclouvain.be>
2024-07-10 14:51:54 +0200

    [FIX] feat(global): Refactoring
    * Removes not used ConfigurationFilesFactory.
    * Fixes some consumers.
    * Fix config files.
    
    Co-authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/configurationFiles/factory/ConfigurationFileFactory.java
+++
@@ -1,7 +0,0 @@
-package org.dspace.uclouvain.configurationFiles.factory;
-
-import org.dspace.uclouvain.configurationFiles.ConfigurationFile;
-
-public interface ConfigurationFileFactory {
-    ConfigurationFile<?> getConfigurationFile(Class klass);
-}
