d861295a136cb42c5836cab1dcb56a62429e3266
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-01-10 09:36:57 +0100

    [UPD] feat(submission): Configure Crossref data import.
    Improved crossref import to import more metadata into the submission form:
    - Publication type import,
    - Change field mapping for journal information,
    - Language import,
    - Date issued and pagination for serial,
    
    Authored-By: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/importer/external/metadatamapping/contributor/CombinedMetadatumContributor.java
+++ dspace-api/src/main/java/org/dspace/importer/external/metadatamapping/contributor/CombinedMetadatumContributor.java
@@ -10,6 +10,7 @@ package org.dspace.importer.external.metadatamapping.contributor;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.dspace.content.DCPersonName;
 import org.dspace.importer.external.metadatamapping.MetadataFieldConfig;
@@ -78,7 +79,10 @@ public class CombinedMetadatumContributor<T> implements MetadataContributor<T> {
         LinkedList<LinkedList<MetadatumDTO>> metadatumLists = new LinkedList<>();
 
         for (MetadataContributor metadatumContributor : metadatumContributors) {
-            LinkedList<MetadatumDTO> metadatums = (LinkedList<MetadatumDTO>) metadatumContributor.contributeMetadata(t);
+            // Convert any Collection to a LinkedList
+            LinkedList<MetadatumDTO> metadatums = (LinkedList<MetadatumDTO>) metadatumContributor.contributeMetadata(t)
+                .stream()
+                .collect(Collectors.toCollection(LinkedList::new));
             metadatumLists.add(metadatums);
         }
         MetadatumDTO[] firstList = new MetadatumDTO[metadatumLists.getFirst().size()];
