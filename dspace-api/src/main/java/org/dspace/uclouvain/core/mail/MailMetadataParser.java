61d8584780183f1949e781e735e9f214468d7db1
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-02-22 09:49:22 +0100

    [ADD] feat(authors-notification): Mail to notify authors of submission
    When a publication is submitted, a mail is sent to all the authors.
    I added a system to get the metadata of the publication in the mail depending on the given language.
    This system keeps the metadata in the same order every time.
    It can also translate a value using a given vocabulary name.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/core/mail/MailMetadataParser.java
@@ -0,0 +1,69 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.core.mail;
+
+import java.util.HashMap;
+
+/**
+ * Main model config class for a mail metadata parser.
+ * This class is used to parse a metadata in order to be displayed in a mail.
+ * 
+ * @author Michaël Pourbaix (michael.pourbaix@uclouvain.be)
+ */
+public class MailMetadataParser {
+    private String metadataField;
+    private HashMap<String, String> labels = new HashMap<>();
+    private String vocabularyName;
+    private boolean multipleValues = true;
+    private String separator = "; ";
+
+    public String getLabel(String lang) {
+        return labels.get(lang);
+    }
+
+    // GETTERS AND SETTERS
+    public HashMap<String, String> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(HashMap<String, String> labels) {
+        this.labels = labels;
+    }
+
+    public String getMetadataField() {
+        return metadataField;
+    }
+
+    public void setMetadataField(String metadataField) {
+        this.metadataField = metadataField;
+    }
+
+    public String getVocabularyName() {
+        return vocabularyName;
+    }
+
+    public void setVocabularyName(String vocabularyName) {
+        this.vocabularyName = vocabularyName;
+    }
+
+    public boolean getMultipleValues() {
+        return multipleValues;
+    }
+
+    public void setMultipleValues(boolean multipleValues) {
+        this.multipleValues = multipleValues;
+    }
+
+    public String getSeparator() {
+        return separator;
+    }
+
+    public void setSeparator(String separator) {
+        this.separator = separator;
+    }
+}
