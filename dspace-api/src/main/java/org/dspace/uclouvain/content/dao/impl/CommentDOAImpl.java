8415a07e8bfe719a8a4706af2d170c73a2978d38
Renaud Michotte <renaud.michotte@uclouvain.be>
2025-03-05 14:02:08 +0100

    [ADD] feat(comment): create comment structure
    Creates "comment" feature:
      * create database migration script.
      * create DOA to interface between application and database
      * create service comment
    
    Authored-by: Renaud Michotte <renaud.mochotte@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/content/dao/impl/CommentDOAImpl.java
@@ -0,0 +1,36 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.content.dao.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+import javax.persistence.Query;
+
+import org.dspace.content.Item;
+import org.dspace.core.AbstractHibernateDAO;
+import org.dspace.core.Context;
+import org.dspace.uclouvain.content.Comment;
+import org.dspace.uclouvain.content.dao.CommentDAO;
+
+@SuppressWarnings("unchecked")
+public class CommentDOAImpl extends AbstractHibernateDAO<Comment> implements CommentDAO {
+
+    @Override
+    public List<Comment> findByItem(Context context, Item item) throws SQLException {
+        Query query = createQuery(context, "SELECT c FROM Comment c WHERE c.owner = :owner");
+        query.setParameter("owner", item);
+        return query.getResultList();
+    }
+
+    @Override
+    public int countCommentsForItem(Context context, Item item) throws SQLException {
+        Query query = createQuery(context, "SELECT count(c) FROM Comment c WHERE c.owner = :owner");
+        query.setParameter("owner", item);
+        return count(query);
+    }
+}
