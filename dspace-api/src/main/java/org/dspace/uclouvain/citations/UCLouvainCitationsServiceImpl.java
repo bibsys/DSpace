3d773faf23b90e15bff298ea253e72ffd5de86cb
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-04-11 14:14:21 +0200

    [ADD] feat(Citations): API for item citations retrieval.
    - Created a new rest endpoint to retrieve either all or a specific citation for an item.
    - New CSL definition for custom uclouvain citation.
    - New system to chose to expose or not a citation formats.
    - New service to generate citation on demand for an item.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/citations/UCLouvainCitationsServiceImpl.java
@@ -0,0 +1,181 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.citations;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dspace.content.Item;
+import org.dspace.content.crosswalk.CrosswalkMode;
+import org.dspace.content.crosswalk.StreamDisseminationCrosswalk;
+import org.dspace.content.integration.crosswalks.StreamDisseminationCrosswalkMapper;
+import org.dspace.content.integration.crosswalks.service.ItemExportFormat;
+import org.dspace.content.integration.crosswalks.service.ItemExportFormatService;
+import org.dspace.content.integration.crosswalks.service.ItemExportFormatServiceImpl;
+import org.dspace.content.service.ItemService;
+import org.dspace.core.Context;
+import org.dspace.utils.DSpace;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Main service to generate text citations for an item.
+ * We use this instead of the classic DSpace system which is using processes and returns files to the user.
+ * 
+ * @author Michaël Pourbaix (michael.pourbaix@uclouvain.be)
+ */
+public class UCLouvainCitationsServiceImpl implements UCLouvainCitationsService {
+    private Logger logger = LogManager.getLogger(UCLouvainCitationsServiceImpl.class);
+    private ItemExportFormatService itemExportFormatService;
+    private StreamDisseminationCrosswalkMapper crosswalkMapper;
+    private static final String CSL_ERROR_REGEX = "\\[CSL STYLE ERROR: .*\\]";
+
+    @Autowired
+    ItemService itemService;
+
+    public boolean isValidFormat(Context context, String citationFormat) {
+        return getCrosswalkMapper().getTypes().contains(citationFormat);
+    }
+
+    /**
+     * Generate a citation for a given format and item.
+     * 
+     * @param context The current DSpace context.
+     * @param item The item to generated a citation for.
+     * @param citationFormat The desired format of the citation.
+     * 
+     */
+    public ItemCitation getCitationForItem(Context context, Item item, String citationFormat)
+        throws UnknownCitationFormatException {
+        // Retrieve the crosswalk for the given format.
+        StreamDisseminationCrosswalk citationCrosswalk = getCrosswalkMapper().getByType(citationFormat);
+        if (citationCrosswalk == null) {
+            throw new UnknownCitationFormatException(
+                citationFormat
+            );
+        }
+
+        if (!citationCrosswalk.canDisseminate(context, item)) {
+            return null;
+        }
+        try {
+            // Write the result in an output stream and then extract the content from it.
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            citationCrosswalk.disseminate(context, item, output);
+            String citationResult = output.toString(StandardCharsets.UTF_8);
+            logger.debug("Processed citation, got: " + citationResult);
+            // The processor may return an error in certain cases.
+            if (containsError(citationResult)) {
+                // If we detect an error: log it and replace the citation result with null.
+                logger.warn(
+                    "Could not generate citation of format: [{}] for item: [{}]. Error is: '{}'.",
+                    citationFormat, item.getID(), citationResult.trim()
+                );
+                citationResult = null;
+            }
+            return new ItemCitation(citationFormat, citationResult);
+        } catch (Exception e) {
+            logger.error(
+                "Citation({}) generation error for {}", citationFormat, item.getID()
+            );
+            return null;
+        }
+    }
+
+    /**
+     * Retrieve all the citation for all the available formats for the given item.
+     * WARNING: It can be very resource demanding if generating a lot of citations. Use wisely.
+     * 
+     * @param context The current DSpace context.
+     * @param item The item to generate citations for.
+     * @return All the possible citations for a given item.
+     */
+    public List<ItemCitation> getAllCitationsForItem(Context context, Item item) {
+        List<ItemCitation> result = new ArrayList<>();
+        for (String format: retrieveCitationsFormats(context, itemService.getEntityType(item))) {
+            try {
+                ItemCitation citation = getCitationForItem(context, item, format);
+                if (citation != null) {
+                    logger.debug(
+                        "Adding citation to final result for format {}, with value '{}'",
+                        format,
+                        citation.getCitation()
+                    );
+                    result.add(citation);
+                }
+            } catch (UnknownCitationFormatException e) {
+                logger.warn(
+                    "Found an unsupported citation format ["
+                    + format + "] in find all format. This is surely not normal.",
+                    e
+                );
+            }
+
+        }
+        return result;
+    }
+
+    /**
+     * Whether or not the citation result contains an error.
+     * @param citation The citation result to evaluate.
+     * @return True if the result contains a string of the form provided by the regex. False otherwise.
+     */
+    private boolean containsError(String citation) {
+        Pattern pattern = Pattern.compile(CSL_ERROR_REGEX);
+        return pattern.matcher(citation).find();
+    }
+
+    /**
+     * Get all valid citations format for a given entityType.
+     * 
+     * @param context The current DSpace context.
+     * @param entityType The entity type to evaluate.
+     * @return All the valid formats for a given entity type.
+     */
+    private List<String> retrieveCitationsFormats(Context context, String entityType) {
+        return getItemExportFormatService().byEntityTypeAndMolteplicity(context, entityType, CrosswalkMode.SINGLE)
+            .stream().map(ItemExportFormat::getId).collect(Collectors.toList());
+    }
+
+    /**
+     * NOTE: METHODS TO RETRIEVE THE EXTERNAL BEANS
+     * 
+     * We need to use a special way of importing for the 2 following beans.
+     * We cannot use autowiring in this case because the beans are defined in another part of the application which is
+     * not always loaded by spring in all the context.
+     * So for example, if we use autowiring it wont work when executing CLI commands.
+     */
+
+    /**
+     * Retrieve the ItemExportFormatService from the DSpace central.
+     * @return An instance of the service retrieved from its bean.
+     */
+    private ItemExportFormatService getItemExportFormatService() {
+        if (itemExportFormatService == null) {
+            // Here note that we need to get the implementation of the service so we add 'Impl'.
+            itemExportFormatService = new DSpace().getSingletonService(ItemExportFormatServiceImpl.class);
+        }
+        return itemExportFormatService;
+    }
+
+    /**
+     * Retrieve the StreamDisseminationCrosswalkMapper from the DSpace central.
+     * @return An instance of the class retrieved from its bean.
+     */
+    private StreamDisseminationCrosswalkMapper getCrosswalkMapper() {
+        if (crosswalkMapper == null) {
+            crosswalkMapper = new DSpace().getSingletonService(StreamDisseminationCrosswalkMapper.class);
+        }
+        return crosswalkMapper;
+    }
+}
