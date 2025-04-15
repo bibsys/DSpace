0a348d58b2b1d5ad92377844fd16d7f0bb92cd05
Renaud Michotte <renaud.michotte@uclouvain.be>
2025-04-15 12:06:23 +0200

    [ADD] feat(sumbission): Adding file access condition acknowledge feature
    A submitter is now forced to validate default access conditions.
    
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/validation/UploadValidator.java
+++ dspace-api/src/main/java/org/dspace/validation/UploadValidator.java
@@ -17,9 +17,11 @@ import java.util.List;
 import org.dspace.app.util.SubmissionStepConfig;
 import org.dspace.content.InProgressSubmission;
 import org.dspace.content.Item;
+import org.dspace.content.MetadataFieldName;
 import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.dspace.core.exception.SQLRuntimeException;
+import org.dspace.services.ConfigurationService;
 import org.dspace.submit.model.UploadConfiguration;
 import org.dspace.submit.model.UploadConfigurationService;
 import org.dspace.validation.model.ValidationError;
@@ -32,30 +34,52 @@ import org.dspace.validation.model.ValidationError;
  */
 public class UploadValidator implements SubmissionStepValidator {
 
+    public static final String DEFAULT_ACCESS_CONDITIONS_ACK_FIELD = "dspace.file-access-condition.acknowledgement";
     private static final String ERROR_VALIDATION_FILEREQUIRED = "error.validation.filerequired";
+    private static final String ERROR_VALIDATION_ACCESSCONDITIONSREQUIRED = "error.validation.accessconditionsrequired";
 
     private ItemService itemService;
-
     private UploadConfigurationService uploadConfigurationService;
-
+    private ConfigurationService configurationService;
     private String name;
 
     @Override
     public List<ValidationError> validate(Context context, InProgressSubmission<?> obj, SubmissionStepConfig config) {
         //TODO MANAGE METADATA
         List<ValidationError> errors = new ArrayList<>();
+        String IdSectionPath = "/" + OPERATION_PATH_SECTIONS + "/" + config.getId();
         UploadConfiguration uploadConfig = uploadConfigurationService.getMap().get(config.getId());
         if (uploadConfig.isRequired() && hasNotUploadedFiles(obj.getItem())) {
-            addError(errors, ERROR_VALIDATION_FILEREQUIRED, "/" + OPERATION_PATH_SECTIONS + "/" + config.getId());
+            addError(errors, ERROR_VALIDATION_FILEREQUIRED, IdSectionPath);
+        }
+        if (isAccessAcknowledgeRequired() && !isFileAccessConditionsValidated(obj.getItem())) {
+            addError(errors, ERROR_VALIDATION_ACCESSCONDITIONSREQUIRED, IdSectionPath);
         }
         return errors;
     }
 
-    public boolean hasNotUploadedFiles(Item item) {
+    private boolean hasNotUploadedFiles(Item item) {
         try {
             return !itemService.hasUploadedFiles(item);
         } catch (SQLException e) {
             throw new SQLRuntimeException(e);
+        }
+    }
+
+    private boolean isAccessAcknowledgeRequired() {
+        return configurationService.getBooleanProperty("webui.submit.upload.acknowledgement.required", false);
+    }
+
+    private boolean isFileAccessConditionsValidated(Item item) {
+        try {
+            MetadataFieldName mdField = new MetadataFieldName(configurationService.getProperty(
+                "webui.submit.upload.acknowledgement.field",
+                DEFAULT_ACCESS_CONDITIONS_ACK_FIELD
+            ));
+            String metadataValue = itemService.getMetadataFirstValue(item, mdField, Item.ANY);
+            return (metadataValue != null && Boolean.parseBoolean(metadataValue));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -73,6 +97,14 @@ public class UploadValidator implements SubmissionStepValidator {
 
     public void setUploadConfigurationService(UploadConfigurationService uploadConfigurationService) {
         this.uploadConfigurationService = uploadConfigurationService;
+    }
+
+    public ConfigurationService getConfigurationService() {
+        return configurationService;
+    }
+
+    public void setConfigurationService(ConfigurationService service) {
+        this.configurationService = service;
     }
 
     @Override
