518a508f34783db7c4983a18d252156645444181
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-05-19 10:20:10 +0200

    [FIX] feat(OSIS-degree): Removed model property that caused problems.
    closes bibsys/DSpace#185
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/external/osis/model/OSISStudentDegree.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/external/osis/model/OSISStudentDegree.java
@@ -20,20 +20,11 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 public class OSISStudentDegree {
     private int anac;
     private String categorieDecret;
-    private int codeCategorieDecret;
-    private int codeEtatInscription;
     private int cycle;
-    private String etatInscription;
-    private String faculte;
-    private String intitOffreAbrege;
     private String intitOffreComplet;
-    private String princSec;
-    private String resultatFinal;
-    private boolean reussite;
     private String sigleOffreComplet;
     private String sigleOffreCompletN;
     private String sigleOffreRacine;
-    private String statut;
     private Optional<String> erreurDossier = Optional.empty();
 
     @JsonIgnore
@@ -57,65 +48,17 @@ public class OSISStudentDegree {
     public void setCategorieDecret(String categorieDecret) {
         this.categorieDecret = categorieDecret;
     }
-    public int getCodeCategorieDecret() {
-        return this.codeCategorieDecret;
-    }
-    public void setCodeCategorieDecret(int codeCategorieDecret) {
-        this.codeCategorieDecret = codeCategorieDecret;
-    }
-    public int getCodeEtatInscription() {
-        return this.codeEtatInscription;
-    }
-    public void setCodeEtatInscription(int codeEtatInscription) {
-        this.codeEtatInscription = codeEtatInscription;
-    }
     public int getCycle() {
         return this.cycle;
     }
     public void setCycle(int cycle) {
         this.cycle = cycle;
     }
-    public String getEtatInscription() {
-        return this.etatInscription;
-    }
-    public void setEtatInscription(String etatInscription) {
-        this.etatInscription = etatInscription;
-    }
-    public String getFaculte() {
-        return this.faculte;
-    }
-    public void setFaculte(String faculte) {
-        this.faculte = faculte;
-    }
-    public String getIntitOffreAbrege() {
-        return this.intitOffreAbrege;
-    }
-    public void setIntitOffreAbrege(String intitOffreAbrege) {
-        this.intitOffreAbrege = intitOffreAbrege;
-    }
     public String getIntitOffreComplet() {
         return this.intitOffreComplet;
     }
     public void setIntitOffreComplet(String intitOffreComplet) {
         this.intitOffreComplet = intitOffreComplet;
-    }
-    public String getPrincSec() {
-        return this.princSec;
-    }
-    public void setPrincSec(String princSec) {
-        this.princSec = princSec;
-    }
-    public String getResultatFinal() {
-        return this.resultatFinal;
-    }
-    public void setResultatFinal(String resultatFinal) {
-        this.resultatFinal = resultatFinal;
-    }
-    public boolean isReussite() {
-        return this.reussite;
-    }
-    public void setReussite(boolean reussite) {
-        this.reussite = reussite;
     }
     public String getSigleOffreComplet() {
         return this.sigleOffreComplet;
@@ -134,12 +77,6 @@ public class OSISStudentDegree {
     }
     public void setSigleOffreRacine(String sigleOffreRacine) {
         this.sigleOffreRacine = sigleOffreRacine;
-    }
-    public String getStatut() {
-        return this.statut;
-    }
-    public void setStatut(String statut) {
-        this.statut = statut;
     }
     public Optional<String> getErreurDossier() {
         return this.erreurDossier;
