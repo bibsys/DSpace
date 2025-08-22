582f91522c01ae1c6aefe97cc13312a858872b18
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-08-22 11:25:12 +0200

    [ADD] feat(esb-client): Client to query ESB endpoint.
    New ESB client with some Digit endpoints to retrieve information about a specific fgs identifier.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/external/esb/client/ESBClient.java
@@ -0,0 +1,19 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.external.esb.client;
+
+import org.dspace.uclouvain.external.esb.model.ESBPersonProfile;
+import org.dspace.uclouvain.external.esb.model.responses.ESBPersonEmailResponse;
+import org.dspace.uclouvain.external.esb.model.responses.ESBPersonMainResponse;
+
+public interface ESBClient {
+    public ESBPersonEmailResponse[] getEmailForFGS(String fgs);
+    public ESBPersonEmailResponse getMainEmailForFGS(String fgs);
+    public ESBPersonMainResponse getDataForFGS(String fgs);
+    public ESBPersonProfile getProfileForFGS(String fgs);
+}
