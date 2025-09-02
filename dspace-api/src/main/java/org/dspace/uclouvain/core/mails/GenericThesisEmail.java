4be473894ee2e6e1420492650c0fac0c3ecfb991
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-09-02 11:32:24 +0200

    [UPD] feat(emails) New system for emails.
    - Improved some details on emails to be more generic
    - Rework of the 'thesis_attestation.error' mail content.
    - Integrated the 'metadata parser' system for metadata list in mails.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/core/mails/GenericThesisEmail.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/core/mails/GenericThesisEmail.java
@@ -7,127 +7,48 @@
  */
 package org.dspace.uclouvain.core.mails;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Stream;
 
-import jakarta.mail.MessagingException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.dspace.content.Item;
-import org.dspace.content.factory.ContentServiceFactory;
-import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.dspace.core.Email;
-import org.dspace.services.ConfigurationService;
-import org.dspace.services.factory.DSpaceServicesFactory;
-import org.dspace.uclouvain.core.utils.MetadataUtils;
+import org.dspace.uclouvain.exceptions.EmailFailedInitException;
 import org.dspace.uclouvain.exceptions.EmailGenerationException;
 
 /**
  * Generic class to create a master thesis email.
+ * In order to use this email, you will need to provide an item of type 'MasterThesis'.
  *
  * @author Michaël Pourbaix (michael.pourbaix@uclouvain.be)
  * @author Renaud Michotte (renaud.michotte@uclouvain.be)
  */
-public abstract class GenericThesisEmail {
+public abstract class GenericThesisEmail extends AbstractUCLouvainEmail {
 
     // ATTRIBUTES ======================================================================================================
-    protected String mailSubject;
-    protected List<String> forcedRecipients;
-    protected HashMap<String, List<String>> metadataMap;
-    protected Item item;
-    protected Context context;
-    protected ConfigurationService configService = DSpaceServicesFactory.getInstance().getConfigurationService();
-    protected ItemService itemService = ContentServiceFactory.getInstance().getItemService();
-    protected String source = configService.getProperty("dspace.dir");
-    protected static final Logger log = LogManager.getLogger(GenericThesisEmail.class);
     protected String supervisorEmailField = configService.getProperty(
             "uclouvain.global.metadata.advisoremail.field", "advisors.email");
     protected String authorEmailField = configService.getProperty(
             "uclouvain.global.metadata.authoremail.field", "authors.email");
 
-    // CONSTRUCTORS ====================================================================================================
-    protected GenericThesisEmail(Context context, Item item) {
-        this.context = context;
-        this.item = item;
-        this.metadataMap = MetadataUtils.getValuesHashMap(item.getMetadata());
-        this.mailSubject = getConfigurationAttribute("subject");
-        this.forcedRecipients = Arrays.asList(getConfigurationAttributes("recipients"));
-    }
-
     // ABSTRACT METHODS ================================================================================================
     protected abstract String getTemplatePath();
     protected abstract String buildMailSubject();
-    protected abstract void generateEmail(Email email) throws EmailGenerationException;
+    protected abstract void generateEmail(Email email, Item item) throws EmailGenerationException;
     protected abstract String getConfigurationName();
     protected abstract List<String> getRecipientAddresses();
 
-    // PUBLIC METHODS ==================================================================================================
-    protected String getConfigurationAttribute(String attribute) {
-        return Arrays.stream(getConfigurationAttributes(attribute)).findFirst().orElse(null);
-    }
-
-    protected String[] getConfigurationAttributes(String attribute) {
-        String propertyKey = String.format("uclouvain.%s.mail.%s", getConfigurationName(), attribute);
-        return configService.getArrayProperty(propertyKey, new String[0]);
+    // METHODS =========================================================================================================
+    public GenericThesisEmail(Context context, Item item) throws EmailFailedInitException {
+        super(context, item);
     }
 
     /**
-     * Get the CC addresses to use for this email.
-     * By default, none address will be used as CC address.
-     *
-     * @return the list of email addresses to use.
+     * Verify that the email can handle a given item.
+     * @param context The current DSpace context.
+     * @param item The item to check the validity of.
+     * @return A boolean indicating if the mail can handle the given item.
      */
-    protected List<String> getCCAddresses() {
-        return Collections.emptyList();
-    }
-
-    /**
-     * Creates and sends an email base on the given configuration (through constructor).
-     * @throws IOException if the creation of the base email using the template was not a success.
-     * @throws EmailGenerationException if the addition of the email arguments failed.
-     * @throws MessagingException if errors occurred during email sent
-     */
-    public void sendEmail() throws IOException, EmailGenerationException, MessagingException {
-        Email email = Email.getEmail(getTemplatePath());
-        email.setSubject(buildMailSubject());
-        email.addArgument(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss")));
-        filterRecipients(getRecipientAddresses()).forEach(email::addRecipient);
-        filterRecipients(getCCAddresses()).forEach(email::addCcAddress);
-        generateEmail(email); // build email content and build additional arguments
-        email.send();
-    }
-
-    // PRIVATE METHODS =================================================================================================
-    /**
-     * Filter recipient addresses bases on configuration.
-     * Either parameter exists into configuration --> in this case, we force to use them (override recipients argument)
-     * Either no special configuration is specified --> filter recipients argument restriction by some domain
-     *
-     * @param recipients the list recipient email addresses
-     * @return a stream of email addresses that can be used.
-     */
-    private Stream<String> filterRecipients(List<String> recipients) {
-        return (!forcedRecipients.isEmpty())
-            ? forcedRecipients.stream()
-            : recipients.stream().filter(this::isValidSuffix);
-    }
-
-    /**
-     * Checks the validity of an address.
-     * An address is valid if it contains a configured suffix.
-     *
-     * @param address the address to validate.
-     * @return True if the address contains at least one of the configured suffixes, 'false' if not.
-     */
-    private Boolean isValidSuffix(String address) {
-        List<String> validEmailSuffix = List.of(this.getConfigurationAttributes("suffixes"));
-        return validEmailSuffix.isEmpty() || validEmailSuffix.stream().anyMatch(address::endsWith);
+    public boolean isValidForItem(Context context, Item item) {
+        return itemService.getEntityType(item).equals("MasterThesis");
     }
 }
