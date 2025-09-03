c08ed6d3e041d07da8020e4dc38c6e739ef80d83
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-09-03 10:15:09 +0200

    [UPD] feat(mails): Adapting author notify email to new system.
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/core/mails/GenericPublicationEmail.java
@@ -0,0 +1,51 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.core.mails;
+
+import java.util.List;
+
+import org.dspace.content.Item;
+import org.dspace.core.Context;
+import org.dspace.core.Email;
+import org.dspace.uclouvain.exceptions.EmailFailedInitException;
+import org.dspace.uclouvain.exceptions.EmailGenerationException;
+
+/**
+ * Generic class to create a publication email.
+ * In order to use this email, you will need to provide an item of type 'Publication'.
+ *
+ * @author Michaël Pourbaix (michael.pourbaix@uclouvain.be)
+ */
+public abstract class GenericPublicationEmail extends AbstractUCLouvainEmail {
+
+    // ATTRIBUTES ======================================================================================================
+    protected String authorEmailField = configService.getProperty(
+            "uclouvain.global.metadata.authoremail.field", "authors.email");
+
+    // ABSTRACT METHODS ================================================================================================
+    protected abstract String getTemplatePath();
+    protected abstract String buildMailSubject();
+    protected abstract void generateEmail(Email email, Item item) throws EmailGenerationException;
+    protected abstract String getConfigurationName();
+    protected abstract List<String> getRecipientAddresses();
+
+    // METHODS =========================================================================================================
+    public GenericPublicationEmail(Context context, Item item) throws EmailFailedInitException {
+        super(context, item);
+    }
+
+    /**
+     * Verify that the email can handle a given item.
+     * @param context The current DSpace context.
+     * @param item The item to check the validity of.
+     * @return A boolean indicating if the mail can handle the given item.
+     */
+    public boolean isValidForItem(Context context, Item item) {
+        return itemService.getEntityType(item).equals("Publication");
+    }
+}
