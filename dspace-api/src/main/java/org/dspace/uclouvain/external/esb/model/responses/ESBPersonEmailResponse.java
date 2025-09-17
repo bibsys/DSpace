d44116b164374db85a267c0fbd9d09ee0c635fa7
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-09-17 16:15:54 +0200

    [UPD] feat(person-event): Email selection logic.
    New logic to select the best possible email for a person having multiple ones.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/external/esb/model/responses/ESBPersonEmailResponse.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/external/esb/model/responses/ESBPersonEmailResponse.java
@@ -9,10 +9,17 @@ package org.dspace.uclouvain.external.esb.model.responses;
 
 /**
  * A model representing a response from Digit email api.
+ * - Each email has an activity: 'A' is for active and 'I' is for inactive.
+ * - Each email has a sorting: 'P' is for primary and 'S' is for secondary.
  * 
  * @author Michaël Pourbaix (michael.pourbaix@uclouvain.be)
  */
 public class ESBPersonEmailResponse extends ESBPersonResponse {
+    public static final String ACTIVITY_ACTIVE_EMAIL = "A";
+    public static final String ACTIVITY_INACTIVE_EMAIL = "I";
+    public static final String SORTING_MAIN_EMAIL = "P";
+    public static final String SORTING_SECONDARY_EMAIL = "S";
+
     private String activity;
     private String emailAddress;
     private String sorting;
