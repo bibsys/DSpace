f92353257f5082be691ffaad5476aa8d5307abee
Renaud Michotte <renaud.michotte@uclouvain.be>
2025-10-15 14:04:59 +0200

    [UPD] feat(general): Improve journal service
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/services/JournalService.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/services/JournalService.java
@@ -10,6 +10,33 @@ package org.dspace.uclouvain.services;
 import org.dspace.core.Context;
 import org.dspace.uclouvain.core.model.Journal;
 
+/**
+ * Service uses to work with `Journal` item object.
+ *
+ * @author Michaël Pourbaix (michael.pourbaix@uclouvain.be)
+ */
 public interface JournalService {
-    public Journal findByIssn(Context context, String issn) throws Exception;
+
+    /** Find a journal by its title.
+     * @param context The current DSpace context.
+     * @param journalName the journal title name to search for; `null` if the journal is not found
+     * @return the corresponding journal; `null` if the journal is not found
+     */
+    Journal findByTitle(Context context, String journalName);
+
+    /**
+     * Find a journal by its ISSN identifier.
+     * @param context The current DSpace context.
+     * @param issn The issn of the journal to search for
+     * @return the corresponding journal; `null` if the journal is not found
+     */
+    Journal findByIssn(Context context, String issn);
+
+    /**
+     * Find a journal by its e-ISSN identifier.
+     * @param context The current DSpace context.
+     * @param eissn The eissn of the journal to search for; `null` if the journal is not found
+     * @return the corresponding journal; `null` if the journal is not found
+     */
+    Journal findByEissn(Context context, String eissn);
 }
