34f87cb913766c17751a7d7f1c856d467cb7124d
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2025-10-16 09:25:29 +0200

    [UPD] feat(profile-creation): IDM filter before Shib profile creation.

--- dspace-api/src/main/java/org/dspace/uclouvain/services/UCLouvainProfileService.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/services/UCLouvainProfileService.java
@@ -11,10 +11,47 @@ import java.util.List;
 
 import org.dspace.content.Item;
 import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
 
 public interface UCLouvainProfileService {
-    public Item findById(Context context, String fgs) throws Exception;
-    public Item findByEmail(Context context, String email) throws Exception;
-    public List<Item> findLinkedPublications(Context context, Item profile) throws Exception;
-    public Item createEmptyProfile(Context context, String fgs) throws Exception;
+
+    /**
+     * Find a profile item using an FGS identifier.
+     * @param context The DSpace application context.
+     * @param fgs     The FGS identifier to use to find the profile.
+     * @return Returns the profile that has the given identifier, null otherwise.
+     */
+    Item findById(Context context, String fgs);
+
+    /**
+     * Find a profile item using an email.
+     * @param context The DSpace application context.
+     * @param email   The email to use to find the profile.
+     * @return Returns the profile that has the given email, null otherwise.
+     */
+    Item findByEmail(Context context, String email);
+
+    /**
+     * For a given profile, retrieve all the linked publications that use this profile has an author.
+     * @param context The DSpace application context.
+     * @param profile The profile to find publications for.
+     * @return Any publication that references this profile has an author.
+     */
+    List<Item> findLinkedPublications(Context context, Item profile);
+
+    /**
+     * Create an empty profile item with only a FGS identifier.
+     * @param context The current DSpace context.
+     * @param fgs     The unique FGS identifier to give to the profile item.
+     */
+    Item createEmptyProfile(Context context, String fgs) throws Exception;
+
+    /**
+     * Create a fresh new profile for a specific user.
+     * Use the metadata present in the given 'currentUser' object to fill the metadata with the profile.
+     * @param context The current DSpace context.
+     * @param person The user to create a profile for.
+     * @return The created profile
+     */
+    Item createNewProfile(Context context, EPerson person) throws Exception;
 }
