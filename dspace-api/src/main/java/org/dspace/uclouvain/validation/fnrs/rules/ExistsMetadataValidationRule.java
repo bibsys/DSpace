bc663ffa3b835300245807e5df1d71bdc92fabda
Renaud Michotte <renaud.michotte@uclouvain.be>
2025-12-03 15:45:12 +0100

    [ADD] feat(api): FNRS validation API
    * Creates FNRS validation logic & configuration file
    * Adds Solr plugin to add fnrs flags for each publication
    * Adds new REST API endpoint "/uclouvain/fnrs/{uuid}"
      - `valid`: quick response to know if document is valid based on FNRS rules
      - `explain`: response with explanations about FNRS validity
    
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/validation/fnrs/rules/ExistsMetadataValidationRule.java
@@ -0,0 +1,54 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.validation.fnrs.rules;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang.StringUtils;
+import org.dspace.content.Item;
+import org.dspace.content.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/** Validation rule to check if some metadata exists into an `Item`
+ *    If multiple metadata fields are defined, any metadata belonging to the item matching metadata field will validate
+ *    this rule.
+ *    Into DSpace, a metadata value cannot be empty or blank string; So no need to check if the item metadata value
+ *    is present, only to check the presence of the metadata field.
+ *
+ * @author Renaud Michotte (renaud.michotte@uclouvain.be)
+ */
+public class ExistsMetadataValidationRule extends ValidationRule {
+
+    // CLASS ATTRIBUTES ================================================================================================
+    @Autowired
+    private ItemService itemService;
+    private List<String> metadataFields;
+    
+    // OVERRIDE METHODS ================================================================================================
+    @Override
+    public boolean validate(Item item) {
+        return metadataFields.stream().anyMatch(mdString -> itemService.hasMetadata(item, mdString));
+    }
+
+    // GETTERS & SETTERS ===============================================================================================
+    public List<String> getMetadataFields() {
+        return metadataFields;
+    }
+    public void setMetadataFields(List<String> metadataFields) {
+        this.metadataFields = metadataFields;
+    }
+    public void setMetadataFields(String metadataFields) {
+        this.metadataFields = Arrays.stream(metadataFields.split(","))
+            .map(String::trim)
+            .filter(StringUtils::isNotBlank)
+            .collect(Collectors.toList());
+    }
+
+}
