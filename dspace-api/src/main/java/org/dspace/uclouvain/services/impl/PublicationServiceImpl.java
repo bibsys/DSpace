dbb16258b928154074ee7f912236877159402275
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2026-01-20 14:54:03 +0100

    [UPD] feat(export-api): Improve API + byAuthor, byAffiliation and byFunding endpoints.
    - Updated endpoints to use the crosswalk id directly.
    - Fixed a case where headers cause crash of the API.
    - Added 3 endpoints to generate exports: byAuthor, byAffiliation and byFunding.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@ucouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/services/impl/PublicationServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/services/impl/PublicationServiceImpl.java
@@ -7,18 +7,32 @@
  */
 package org.dspace.uclouvain.services.impl;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 import static org.dspace.content.authority.Choices.CF_ACCEPTED;
 import static org.dspace.content.authority.Choices.CF_UNSET;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.dspace.content.Item;
 import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
+import org.dspace.discovery.DiscoverQuery;
+import org.dspace.discovery.DiscoverResult;
+import org.dspace.discovery.SearchService;
+import org.dspace.discovery.SearchServiceException;
+import org.dspace.discovery.indexobject.IndexableItem;
+import org.dspace.uclouvain.core.model.OrgUnit;
 import org.dspace.uclouvain.core.model.exceptions.PublicationSetAuthorException;
 import org.dspace.uclouvain.core.model.publication.Publication;
 import org.dspace.uclouvain.core.model.publication.PublicationAuthor;
+import org.dspace.uclouvain.core.model.publication.PublicationFactory;
 import org.dspace.uclouvain.services.PublicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -26,6 +40,10 @@ public class PublicationServiceImpl implements PublicationService {
 
     @Autowired
     ItemService itemService;
+    @Autowired
+    SearchService searchService;
+
+    // PUBLIC METHODS ==================================================================================================
 
     public PublicationAuthor setAuthor(Context context, Publication publication,
             String name, String email, String orcid, String fgs,
@@ -84,4 +102,58 @@ public class PublicationServiceImpl implements PublicationService {
         }
     }
 
+    public Stream<Publication> findByAuthors(
+        Context context, List<Item> authors
+    ) throws SearchServiceException {
+        String query = authors.stream()
+            .map(author -> "isAuthorOfPublication:\"%s\"".formatted(author.getID().toString()))
+            .collect(Collectors.joining(" OR "));
+        return findPublications(context, query, new HashMap<>());
+    }
+
+    public Stream<Publication> findByAffiliations(
+        Context context, List<OrgUnit> entities) throws SearchServiceException {
+        String query = entities.stream()
+            .map(entity -> "isOrgUnitOfPublication:\"%s\"".formatted(entity.getID().toString()))
+            .collect(Collectors.joining(" OR "));
+        return findPublications(context, query, new HashMap<>());
+    }
+
+    public Stream<Publication> findByFunding(
+        Context context, String fundingOrganization, String fundingProgram
+    ) throws SearchServiceException {
+        String query = "funding.organization:\"%s\"".formatted(fundingOrganization);
+        Map<String, String> filterQueries = new HashMap<>();
+        if (isNotBlank(fundingProgram)) {
+            filterQueries.put("funding.program", fundingProgram);
+        }
+        return findPublications(context, query, filterQueries);
+    }
+
+    public Stream<Publication> findPublications(
+        Context context, String query, Map<String, String> filterQueries
+    ) throws SearchServiceException {
+        DiscoverQuery dq = new DiscoverQuery();
+        dq.addDSpaceObjectFilter(IndexableItem.TYPE);
+        dq.setQuery(query);
+        dq.setMaxResults(SearchService.MAX_RESULT);
+        filterQueries.entrySet().forEach(entry -> {
+            dq.addFilterQueries("%s:\"%s\"".formatted(entry.getKey(), entry.getValue()));
+        });
+        DiscoverResult searchResult = searchService.search(context, dq);
+        return searchResult.getIndexableObjects()
+            .stream()
+            .map(indexableObject -> buildPublication(((IndexableItem) indexableObject).getIndexedObject()))
+            .filter(Objects::nonNull);
+    }
+
+    // PRIVATE METHODS =================================================================================================
+
+    private Publication buildPublication(Item item) {
+        try {
+            return PublicationFactory.build(item);
+        } catch (Exception ignored) {
+            return null;
+        }
+    }
 }
