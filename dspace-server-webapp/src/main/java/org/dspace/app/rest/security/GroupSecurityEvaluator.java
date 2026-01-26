42b54cf423483f25320fe66927d86f4538762ecf
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2026-01-26 14:43:37 +0100

    [ADD] feat(solr-search): API endpoint to query Solr for publications search.
    Added a new endpoint to query SOLR in order to find publications.
    The endpoint only returns publication items that are archived, discoverable and not withdrawn.
    The client can specify a query, some filter queries, a page and a size.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/GroupSecurityEvaluator.java
@@ -0,0 +1,69 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.security;
+
+import java.sql.SQLException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.dspace.app.rest.utils.ContextUtil;
+import org.dspace.authorize.service.AuthorizeService;
+import org.dspace.core.Context;
+import org.dspace.eperson.Group;
+import org.dspace.eperson.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Group security component to check for group membership.
+ * 
+ * @author Michaël Pourbaix <michael.pourbaix@uclouvain.be>
+ */
+@Component(value = "groupSecurity")
+public class GroupSecurityEvaluator {
+    @Autowired
+    private GroupService groupService;
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private AuthorizeService authorizeService;
+
+    /**
+     * Check whether the current DSpace user is a member of at least one
+     * of the given groups.
+     * 
+     * An admin user is considered a member of any group.
+     *
+     * Intended for use in Spring Security SpEL expressions.
+     *
+     * @param groupNames one or more group names
+     * @return true if the current user belongs to any of the groups,
+     *         false otherwise or in case of error
+     */
+    public boolean isMemberOf(String... groupNames) {
+        Context context = ContextUtil.obtainContext(request);
+
+        if (context == null || context.getCurrentUser() == null) {
+            return false;
+        }
+
+        try {
+            if (authorizeService.isAdmin(context)) {
+                return true;
+            }
+            for (String groupName : groupNames) {
+                Group group = groupService.findByName(context, groupName);
+                if (group != null && groupService.isMember(context, context.getCurrentUser(), group)) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            return false;
+        }
+        return false;
+    }
+}
