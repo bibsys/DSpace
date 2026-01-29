1f4b7505f4c8f9a062f10b3b37fb334be207a17d
Renaud Michotte <renaud.michotte@uclouvain.be>
2026-01-29 11:45:59 +0100

    [UPD] feat(api): Improve citations generation and API endpoints
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/citations/ItemCitations.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/citations/ItemCitations.java
@@ -7,26 +7,12 @@
  */
 package org.dspace.uclouvain.citations;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Internal class for working with a list of item citations. Very similar to the Rest model.
  * 
  * @author Michaël Pourbaix (michael.pourbaix@uclouvain.be)
  */
-public class ItemCitations {
-    List<ItemCitation> citations = new ArrayList<>();
-
-    public List<ItemCitation> getCitations() {
-        return this.citations;
-    }
-
-    public void setCitations(List<ItemCitation> citations) {
-        this.citations = citations;
-    }
-
-    public void addCitation(String format, String value) {
-        this.citations.add(new ItemCitation(format, value));
-    }
-}
+public record ItemCitations(UUID id, List<CitationEntry> citations) { }
