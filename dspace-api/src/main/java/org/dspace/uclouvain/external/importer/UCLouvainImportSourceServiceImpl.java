47a3c7cba340b41f9a1b0bfa2ebec48362c095c7
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2026-02-17 13:32:59 +0100

    [ADD] feat(pubmed-import): Custom crossref import provider.
    Added a new custom import provider that uses a given pubmed id to retrieve information from pubmed and auto-complete form.
    This provider can potentially search into DSpace to find matching author or journal to enrich the metadata of the publication.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/external/importer/UCLouvainImportSourceServiceImpl.java
@@ -0,0 +1,74 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.external.importer;
+
+import static org.dspace.content.authority.Choices.CF_UNSET;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.dspace.content.dto.MetadataValueDTO;
+import org.dspace.core.CrisConstants;
+import org.dspace.uclouvain.core.model.MetadataField;
+
+public abstract class UCLouvainImportSourceServiceImpl implements UCLouvainImportSourceService {
+    public abstract List<MetadataValueDTO> getMetadataList(String query);
+
+    /**
+     * Adds a metadata to the given metadata list.
+     * If the placeholder flag is set to true and the value is empty, add a metadata placeholder.
+     * If the placeholder flag is set to false and the value is empty, don't add any metadata.
+     * 
+     * @param list The complete metadata list to amend the metadata to.
+     * @param field The metadata field of the metadata to create.
+     * @param value The value of the metadata to create.
+     * @param authority The authority of the metadata to create.
+     * @param confidence The confidence of the metadata to create.
+     * @param placeholder Whether to use a placeholder for the value of the metadata if given value is empty.
+     */
+    protected void addMetadata(
+        List<MetadataValueDTO> list, String field, String value, String authority, int confidence, boolean placeholder
+    ) {
+        boolean isBlank = StringUtils.isEmpty(value);
+        if (isBlank && !placeholder) {
+            return;
+        }
+
+        value = isBlank ? CrisConstants.PLACEHOLDER_PARENT_METADATA_VALUE : value;
+        authority = isBlank ? null : authority;
+        confidence = isBlank ? CF_UNSET : confidence;
+
+        MetadataField md = new MetadataField(field);
+        MetadataValueDTO dto = new MetadataValueDTO();
+
+        dto.setSchema(md.getSchema());
+        dto.setElement(md.getElement());
+        dto.setQualifier(md.getQualifier());
+        dto.setValue(value);
+        dto.setAuthority(authority);
+        dto.setConfidence(confidence);
+
+        list.add(dto);
+    }
+
+    /**
+     * Same as 'addMetadata' but for a list of values.
+     * @param list The complete metadata list to amend the metadata to.
+     * @param field The metadata field of the metadata to create.
+     * @param values The values of the metadata to create.
+     * @param authority The authority of the metadata to create.
+     * @param confidence The confidence of the metadata to create.
+     * @param placeholder Whether to use a placeholder for the value of the metadata if given value is empty.
+     */
+    protected void addAllMetadata(
+        List<MetadataValueDTO> list, String field, List<String> values,
+        String authority, int confidence, boolean placeholder
+    ) {
+        values.forEach(val -> addMetadata(list, field, val, authority, confidence, placeholder));
+    }
+}
