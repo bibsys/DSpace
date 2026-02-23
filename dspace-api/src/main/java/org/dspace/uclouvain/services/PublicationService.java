f938bcda742ff3d0b1fb37ca0eb85f13664a2252
Renaud Michotte <renaud.michotte@uclouvain.be>
2026-02-23 10:42:25 +0100

    [UPD] feat(auth): Change "withdrawn" feature behavior
    Allow item owner, submitter, authors and app managers to allow withdrawn/reinstate an item depending on entityType+docMainType.
    !! Doctoral dissertation thesis can never be withdrawn (only admin)
    
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- dspace-api/src/main/java/org/dspace/uclouvain/services/PublicationService.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/services/PublicationService.java
@@ -7,11 +7,13 @@
  */
 package org.dspace.uclouvain.services;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.Item;
 import org.dspace.core.Context;
 import org.dspace.discovery.SearchServiceException;
@@ -21,76 +23,113 @@ import org.dspace.uclouvain.core.model.publication.Publication;
 import org.dspace.uclouvain.core.model.publication.PublicationAuthor;
 
 public interface PublicationService {
+
     /**
      * Add a new author to a publication (item) metadata at a specific place.
      * 
-     * @param context     The current DSpace context.
-     * @param name        The name of the author.
-     * @param email       The email of the author.
-     * @param orcid       The ORCID of the author.
-     * @param fgs         The fgs identifier of the author.
-     * @param institution The institution of the author.
-     * @param role        The role of the author in the publication.
-     * @param authority   The UUID of the profile item.
-     * @param place       The place pf the author in the author list.
-     * @return A PublicationAuthor object containing all the informations.
-     * @throws PublicationSetAuthorException
+     * @param context     The current DSpace context
+     * @param name        The name of the author
+     * @param email       The email of the author
+     * @param orcid       The ORCID of the author
+     * @param fgs         The fgs identifier of the author
+     * @param institution The institution of the author
+     * @param role        The role of the author in the publication
+     * @param authority   The UUID of the profile item
+     * @param place       The place pf the author in the author list
+     * @return the corresponding publication author
+     * @throws PublicationSetAuthorException if any error occurred while persisting information in publication.
      */
-    public PublicationAuthor setAuthor(Context context, Publication publication, String name, String email,
+    default PublicationAuthor setAuthor(Context context, Publication publication, String name, String email,
             String orcid, String fgs, String institution, String role, UUID authority, Integer place)
-            throws PublicationSetAuthorException;
+            throws PublicationSetAuthorException {
+        PublicationAuthor author = new PublicationAuthor()
+            .setName(name)
+            .setEmail(email)
+            .setOrcidID(orcid)
+            .setFgs(fgs)
+            .setRole(role)
+            .setInstitution(institution)
+            .setAuthority(authority)
+            .setPlace(place);
+        this.setAuthor(context, publication, author);
+        return author;
+    }
 
     /**
      * Set author information in the publication (item) metadata.
      * 
-     * @param context     The current DSpace context.
-     * @param publication The publication to set an author for.
-     * @param author      The author to persist.
-     * @throws PublicationSetAuthorException if any error occurres while persisting
-     *                                       information in publication.
+     * @param context     The current DSpace context
+     * @param publication The publication to set an author for
+     * @param author      The author to persist
+     * @return the corresponding publication author
+     * @throws PublicationSetAuthorException if any error occurred while persisting information in publication.
      */
-    public void setAuthor(Context context, Publication publication, PublicationAuthor author)
+    PublicationAuthor setAuthor(Context context, Publication publication, PublicationAuthor author)
             throws PublicationSetAuthorException;
 
     /**
      * Find all publication linked to at least one of the given author.
-     * @param context The current DSpace application context.
-     * @param authors A list of authors to find publications for.
-     * @return An stream of all the found publications.
-     * @throws SearchServiceException If any solr exception occurres while searching.
+     *
+     * @param context The current DSpace application context
+     * @param authors A list of authors to find publications for
+     * @return stream containing all found publications
+     * @throws SearchServiceException If any solr exception occurred while searching.
      */
-    public Stream<Publication> findByAuthors(Context context, List<Item> authors) throws SearchServiceException;
+    Stream<Publication> findByAuthors(Context context, List<Item> authors) throws SearchServiceException;
 
     /**
      * Find all publication linked to at least one of the given affiliation.
-     * @param context The current DSpace application context.
-     * @param affiliations A list of affiliation to find publications for.
-     * @return An stream of all the found publications.
-     * @throws SearchServiceException If any solr exception occurres while searching.
+     *
+     * @param context The current DSpace application context
+     * @param affiliations A list of affiliation to find publications for
+     * @return A stream of all the found publications
+     * @throws SearchServiceException If any solr exception occurred while searching
      */
-    public Stream<Publication> findByAffiliations(Context context, List<OrgUnit> affiliations)
+    Stream<Publication> findByAffiliations(Context context, List<OrgUnit> affiliations)
         throws SearchServiceException;
 
     /**
      * Find all publication linked to at specific funding.
-     * @param context The current DSpace application context.
-     * @param fundingOrganization The organization of the funding (required).
-     * @param fundingProgram The program of the funding (optional).
-     * @return A stream of all the publications linked to the given funding.
-     * @throws SearchServiceException
+     *
+     * @param context The current DSpace application context
+     * @param fundingOrganization The organization of the funding (required)
+     * @param fundingProgram The program of the funding (optional)
+     * @return A stream of all the publications linked to the given funding
+     * @throws SearchServiceException If any solr exception occurred while searching
      */
-    public Stream<Publication> findByFunding(Context context, String fundingOrganization, String fundingProgram)
+    Stream<Publication> findByFunding(Context context, String fundingOrganization, String fundingProgram)
         throws SearchServiceException;
 
     /**
      * Find all publication items matching the given query and filter queries.
      * TODO: Improve this logic to handle more params (sort, filters...). It would be better to externalize this code.
-     * @param context The current DSpace context.
-     * @param query The main query to match.
-     * @param filterQueries Additional filter queries to match.
-     * @return A stream of all found publications based on the given query.
-     * @throws SearchServiceException
+     *
+     * @param context The current DSpace context
+     * @param query The main query to match
+     * @param filterQueries Additional filter queries to match
+     * @return A stream of all found publications based on the given query
+     * @throws SearchServiceException If any solr exception occurred while searching
      */
-    public Stream<Publication> findPublications(Context context, String query, Map<String, String> filterQueries)
+    Stream<Publication> findPublications(Context context, String query, Map<String, String> filterQueries)
         throws SearchServiceException;
+
+    /**
+     * Determine if the current logged user is an author of the publication
+     *
+     * @param context The current DSpace context
+     * @param item the item to analyze (should ba a publication)
+     * @return True if any publication author is linked to the current logged user
+     * @throws SQLException if any database exception occurred
+     * @throws AuthorizeException if any authorization exception occurred
+     */
+    boolean isAuthorOfPublication(Context context, Item item) throws SQLException, AuthorizeException;
+
+    /**
+     * Determine if the item could be retired depending on the context (if the item is a publication)
+     *
+     * @param context the current DSpace context
+     * @param item the item to analyze
+     * @return true if the item can be withdrawn; false otherwise (or any exception occurred)
+     */
+    boolean authorizeWithdrawItem(Context context, Item item);
 }
