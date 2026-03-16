20b03badf43f9c03537722e1d989503271283d9b
Renaud Michotte <renaud.michotte@uclouvain.be>
2026-03-16 13:25:52 +0100

    [ADD] feat(api): Add "hasRoleDelegator" feature
    Authored-by: Renaud Michotte <renaud.michotte@uclouvain.be>

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/EPersonRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/EPersonRestRepository.java
@@ -244,7 +244,7 @@ public class EPersonRestRepository extends DSpaceObjectRestRepository<EPerson, E
     }
 
     @Override
-    @PreAuthorize("@groupSecurity.isManager() || hasPermission(#id, 'EPERSON', 'READ')")
+    @PreAuthorize("@groupSecurity.isManager() || @groupSecurity.isDelegator() || hasPermission(#id, 'EPERSON', 'READ')")
     public EPersonRest findOne(Context context, UUID id) {
         EPerson eperson = null;
         try {
