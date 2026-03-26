071f3b71169bc00fe4aadb398e63926e679e230e
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2026-03-26 16:02:38 +0100

    [UPD] feat(profile-ingest-logging) Improve logging at profile ingest.

--- dspace-api/src/main/java/org/dspace/uclouvain/profileIngester/services/IDMPersonValidityServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/uclouvain/profileIngester/services/IDMPersonValidityServiceImpl.java
@@ -8,15 +8,13 @@
 package org.dspace.uclouvain.profileIngester.services;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.dspace.uclouvain.configurationFiles.files.IDMPersonFilterConfigurationFile;
 import org.dspace.uclouvain.external.esb.client.ESBClient;
 import org.dspace.uclouvain.external.esb.model.responses.ESBPersonIDMMembershipResponse;
+import org.dspace.uclouvain.profileIngester.exceptions.IDMCheckException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -32,26 +30,24 @@ public class IDMPersonValidityServiceImpl implements IDMPersonValidityService {
     @Autowired
     IDMPersonFilterConfigurationFile idmPersonFilterConfigurationFile;
 
-    private static final Logger log = LogManager.getLogger(IDMPersonValidityServiceImpl.class);
-
     /**
      * Check if a given person has a valid IDM membership.
      * 
      * @param fgs the FGS identifier of the person to check validity of.
      * @return true if the user has a valid IDM membership, false otherwise.
      */
-    public boolean isPersonIDMValid(String fgs) {
+    public boolean isPersonIDMValid(String fgs) throws IDMCheckException {
         List<Integer> idmEntries = getIDMEntriesForFGS(fgs);
         return isPersonIDMValid(idmEntries);
     }
 
-    public boolean isPersonIDMValid(List<Integer> idmEntries) {
+    public boolean isPersonIDMValid(List<Integer> idmEntries) throws IDMCheckException {
         if (!idmEntries.isEmpty()) {
             try {
                 List<Integer> filters = idmPersonFilterConfigurationFile.getData();
                 return filters.stream().anyMatch(filter -> idmEntries.contains(filter));
             } catch (IOException e) {
-                log.error("Could not read IDM filters config file to check for person validity.", e);
+                throw new IDMCheckException("Could not read IDM filters config file.", e);
             }
         }
         return false;
@@ -63,10 +59,11 @@ public class IDMPersonValidityServiceImpl implements IDMPersonValidityService {
      * @param fgs The FGS identifier of the user to get IDM information of.
      * @return A list of IDM ids of which the user is a member of.
      */
-    public List<Integer> getIDMEntriesForFGS(String fgs) {
+    public List<Integer> getIDMEntriesForFGS(String fgs) throws IDMCheckException {
         ESBPersonIDMMembershipResponse[] memberships = esbClient.getIDMMembershipsForFGS(fgs);
-        return (memberships != null)
-            ? Arrays.stream(memberships).map(membership -> membership.getGridNumber()).toList()
-            : new ArrayList<>();
+        if (memberships == null) {
+            throw new IDMCheckException("No valid response from ESB for IDM check.");
+        }
+        return Arrays.stream(memberships).map(membership -> membership.getGridNumber()).toList();
     }
 }
