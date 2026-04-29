3dc075aacf1e0fe6f0440653ceb6d47b596e17bc
Michaël Pourbaix <michael.pourbaix@uclouvain.be>
2026-04-29 09:59:52 +0200

    [ADD] feat(solr): Endpoint to index a given item.
    A new endpoint allows to fully index (or re-index) a given item.
    
    Authored-by: Michaël Pourbaix <michael.pourbaix@uclouvain.be>

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/SearchRestController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/SearchRestController.java
@@ -8,21 +8,30 @@
 package org.dspace.app.rest;
 
 import java.util.List;
+import java.util.UUID;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.dspace.app.rest.exception.InvalidSearchRequestException;
 import org.dspace.app.rest.utils.ContextUtil;
 import org.dspace.authorize.service.AuthorizeService;
+import org.dspace.content.Item;
+import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
+import org.dspace.discovery.IndexableObject;
+import org.dspace.discovery.IndexingService;
 import org.dspace.discovery.SearchServiceException;
+import org.dspace.discovery.indexobject.factory.IndexObjectFactoryFactory;
 import org.dspace.eperson.service.GroupService;
+import org.dspace.services.factory.DSpaceServicesFactory;
 import org.dspace.uclouvain.search.model.SolrSearchResponse;
 import org.dspace.uclouvain.search.services.UCLouvainSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +45,13 @@ public class SearchRestController {
     GroupService groupService;
     @Autowired
     AuthorizeService authorizeService;
+    @Autowired
+    ItemService itemService;
+
+    IndexingService indexer = DSpaceServicesFactory.getInstance().getServiceManager()
+                                                   .getServiceByName(IndexingService.class.getName(),
+                                                                     IndexingService.class);
+    IndexObjectFactoryFactory indexObjectServiceFactory = IndexObjectFactoryFactory.getInstance();
 
     // MAIN ENDPOINTS --------------------------------------------------------------------------------------------------
 
@@ -56,6 +72,41 @@ public class SearchRestController {
             return ResponseEntity.ok(res);
         } catch (SearchServiceException e) {
             throw new InvalidSearchRequestException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Fully-re-index a given item in Solr.
+     */
+    @PutMapping("/index/{uuid}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<?> index(
+        HttpServletResponse response, HttpServletRequest request,
+        @PathVariable UUID uuid
+    ) {
+        Context context = ContextUtil.obtainContext(request);
+        try {
+            Item item = itemService.find(context, uuid);
+            if (item == null) {
+                return ResponseEntity.badRequest()
+                    .body("Item not found for %s".formatted(uuid.toString()));
+            }
+
+            IndexableObject io = indexObjectServiceFactory.getIndexableObjects(context, item).stream()
+                .findFirst()
+                .orElse(null);
+
+            if (io == null) {
+                return ResponseEntity.badRequest()
+                    .body("Cannot find an indexable object for %s".formatted(uuid.toString()));
+            }
+
+            indexer.indexContent(context, io, true, true);
+
+            return ResponseEntity.ok("%s indexed".formatted(uuid.toString()));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError()
+                .body("Could not index item %s :: %s".formatted(uuid.toString(), e.getMessage()));
         }
     }
 }
