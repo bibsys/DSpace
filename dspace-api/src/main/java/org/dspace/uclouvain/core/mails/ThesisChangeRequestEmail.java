6a4c8715c20e4496970304bb6d0bc77f1b842a75
Renaud Michotte <renaud.michotte@uclouvain.be>
2026-05-07 13:58:49 +0200

    [ADD] feat(i18n): Translate submission form and licence.
    Co-Authored-By: Renaud Michotte <renaud.michotte@uclouvain.be>

---
+++ dspace-api/src/main/java/org/dspace/uclouvain/core/mails/ThesisChangeRequestEmail.java
@@ -0,0 +1,76 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.core.mails;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.dspace.content.Item;
+import org.dspace.core.Email;
+import org.dspace.uclouvain.core.GenericThesisEmail;
+import org.dspace.uclouvain.core.model.MetadataField;
+import org.dspace.uclouvain.exceptions.EmailGenerationException;
+
+/**
+ * Class representing the ChangeRequest email. This is email is to be sent when a manager requests a change
+ * for a workflow item.
+ * It will send the email along with the data to both the submitter and the promoters.
+ * 
+ * @author Michaël Pourbaix (michael.pourbaix@uclouvain.be)
+ */
+public class ThesisChangeRequestEmail extends GenericThesisEmail {
+    protected String changeRequest;
+
+    protected String promoterEmailField = new MetadataField(
+            configService.getProperty("uclouvain.global.metadata.advisoremail.field", "advisors.email")
+    ).getFullString("_");
+
+    public ThesisChangeRequestEmail(Item item, String reason) {
+        super(item);
+        this.changeRequest = reason;
+    }
+
+    /**
+     * Get the author email addresses that will be used as recipients.
+     * @return The recipients addresses.
+     */
+    protected List<String> getRecipientsEmails() {
+        List<String> recipients =  this.metadataMap.get(this.promoterEmailField);
+        recipients.add(this.item.getSubmitter().getEmail());
+        return recipients;
+    }
+
+    @Override
+    protected String getConfigurationName() {
+        return "change_request";
+    }
+
+    @Override
+    protected String getTemplatePath() {
+        return this.source + "/config/emails/change_request_notify_author";
+    }
+
+    /**
+     * Fill the email with information: recipients, subjects and arguments for the template.
+     * @param email the email to fill.
+     * @throws EmailGenerationException: If an error occurs while filling email information.
+     */
+    @Override
+    protected void generateEmail(Email email) throws EmailGenerationException {
+        try {
+            this.addRecipients(this.getRecipientsEmails(), email);
+            email.addArgument(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss")));
+            email.addArgument(this.metadataMap.get("dc_title").get(0));
+            email.addArgument(this.changeRequest);
+            email.setSubject(this.mailSubject);
+        } catch (Exception e) {
+            throw new EmailGenerationException("An error occurred while filling email informations.", e);
+        }
+    }
+}
---
+++ dspace-api/src/main/java/org/dspace/uclouvain/core/mails/ThesisSupervisorAttestationEmail.java
@@ -0,0 +1,102 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.uclouvain.core.mails;
+
+import static org.dspace.core.Constants.CONTENT_BUNDLE_NAME;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dspace.content.Bitstream;
+import org.dspace.content.Item;
+import org.dspace.core.Email;
+import org.dspace.uclouvain.core.directLink.DirectLinkGenerator;
+import org.dspace.uclouvain.core.directLink.DirectLinkGeneratorFactory;
+import org.dspace.uclouvain.core.model.MetadataField;
+import org.dspace.uclouvain.exceptions.EmailGenerationException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Main class to send an email for the submission attestation to the promoters of the item.
+ * This mail is sent when someone makes a new submission, and it enters the workflow validation system.
+ * This class extends {@link ThesisAuthorAttestationEmail} and adds the generation of file access links for promoters.
+ * 
+ * @author Michaël Pourbaix (michael.pourbaix@uclouvain.be)
+ */
+public class ThesisSupervisorAttestationEmail extends ThesisAuthorAttestationEmail {
+
+    private final Logger logger = LogManager.getLogger(ThesisSupervisorAttestationEmail.class);
+
+    @Autowired
+    private DirectLinkGeneratorFactory directLinkGeneratorFactory;
+
+    protected String supervisorEmailField = new MetadataField(
+            configService.getProperty("uclouvain.global.metadata.advisoremail.field", "advisors.email")
+        ).getFullString("_");
+    protected String backendURL = configService.getProperty("dspace.server.url");
+
+    public ThesisSupervisorAttestationEmail(Item item, InputStream attachment) {
+        super(item, attachment);
+    }
+
+    @Override
+    protected void generateEmail(Email email) throws EmailGenerationException {
+        super.generateEmail(email);
+        this.appendUrlsToEmail(this.metadataMap, email, this.item);
+    }
+
+    /**
+     * Get the corresponding template file for the promoter attestation mail.
+     */
+    @Override
+    protected String getTemplatePath() {
+        return this.source + "/config/emails/pdf_attestation_promoter";
+    }
+
+    /**
+     * Get the promoter email addresses that will be used as recipients.
+     * @return The recipients addresses.
+     */
+    @Override
+    protected List<String> getRecipientsEmails() {
+        return this.metadataMap.get(supervisorEmailField);
+    }
+
+    /**
+     * Used for supervisor emails, appends access URLs for the bitstreams to the email.
+     * @param metadata a HashMap containing all the metadata of the submission.
+     * @param email the email object to append the URLs to.
+     * @param dspaceItem the DSpace item corresponding to the submission.
+     */
+    protected void appendUrlsToEmail(HashMap<String, List<String>> metadata, Email email, Item dspaceItem) {
+        List<String> urls = new ArrayList<>();
+        try {
+            String supervisorEmail = metadata.get(supervisorEmailField).get(0);
+            if (supervisorEmail == null) {
+                throw new Exception("Tried to generate access URLs for the promoters but no email was found.");
+            }
+            DirectLinkGenerator linkGenerator = directLinkGeneratorFactory.getGenerator("thesisSupervisor");
+            List<Bitstream> bitstreams = item.getBundles(CONTENT_BUNDLE_NAME)
+                    .stream()
+                    .flatMap(bundle -> bundle.getBitstreams().stream())
+                    .toList();
+            for (Bitstream bitstream : bitstreams) {
+                urls.add(linkGenerator.buildURL(bitstream, Map.of("email", supervisorEmail)));
+            }
+        } catch (Exception e) {
+            logger.error("Unable to generate direct download URLs for supervisors :: " + e.getMessage());
+        } finally {
+            email.addArgument(urls);
+        }
+    }
+}
